77a869eed94f267f238b1ac03b3cf3ee1caca86a
Shelly Ren <rens@ornl.gov>
2012-12-21 11:06:59 -0500

    created an instrument conversion table from long name to short name

--- scripts/autoReposSync.sh
+++ scripts/autoReposSync.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+function createInstrumentHash() {
+  iList["ARCS"]="ARCS"
+  iList["BASIS"]="BSS"
+  iList["CNCS"]="CNCS"
+  iList["HYSPEC"]="HYS"
+  iList["NOMAD"]="NOM"
+  iList["POWGEN"]="PG3"
+  iList["REF_L"]="REF_L"
+  iList["REF_M"]="REF_M"
+  iList["SEQUOIA"]="SEQ"
+}
+
 function diffScript() {
 file1=$1
 file2=$2
-echo $file1
-echo $file2
 diff $file1 $file2 > /dev/null 2>&1
 ret="$?"
 if [ "$ret" -eq 2 ]; then
@@ -18,25 +28,19 @@ else
 fi
 }
 
+
+declare -A iList 
+createInstrumentHash
+
 for file in /SNS/users/3qr/workspace/projects/autoreduce/autoreduce/SNSReductionScripts/*; do
-  echo $file
-  instrument=`echo ${file##*/} |tr 'a-z' 'A-Z'`
-  if [ $instrument == "BASIS" ]; then
-    instrument="BSS"
-  elif [ $instrument == "HYSPEC" ]; then
-    instrument="HYS"
-  elif [ $instrument == "POWGEN" ]; then
-    instrument="PG3"
-  elif [ $instrument == "SEQUOIA" ]; then
-    instrument="SEQ"
-  fi
-  echo $instrument
+  #echo $file
+  inst=`echo ${file##*/} |tr 'a-z' 'A-Z'`
+  instrument=${iList[$inst]}
   reduceScript="reduce_$instrument.py"
-  #echo $reduceScript
+  echo $reduceScript
   file1="$file/$reduceScript"
   file2="/SNS/"$instrument"/shared/autoreduce/$reduceScript"
-  if [ ! -f $file1 ]; then
-    echo "$file1 does not exist, nothing to do."
+  if [ ! -f $file1 ]; then echo "$file1 does not exist, nothing to do."
   else
     if [ ! -f $file2 ]; then
       echo "$file2 does not exist, nothing to do."
@@ -47,16 +51,16 @@ for file in /SNS/users/3qr/workspace/projects/autoreduce/autoreduce/SNSReduction
 done
 
 echo
-echo "List of files to be updated in git:"${updateList[@]}
+echo "Number of files to be updated in git: "${#updateList[*]}
 if [[ ${#updateList[*]} -ne 0 ]]; then
   for file in ${updateList[@]}; do
     echo $file
     git add $file
   done
+  git commit -m 'Updated reduction script by cron job'
+  git push
 fi
 
-git commit -m 'Updated reduction script by cron job'
-git push
 
 
 
