525a93e91ca2aa2f196f694d32b3105b3dd1a2bf
Stuart Campbell <campbellsi@ornl.gov>
2013-04-12 16:46:33 -0400

    Add changes from Andrei and Shelly

--- SNSReductionScripts/hyspeca/reduce_HYSA.py
+++ SNSReductionScripts/hyspeca/reduce_HYSA.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
-import os
-import sys
+import os, sys, traceback
 from numpy import *
 #from MaskBTP import *
 
@@ -14,8 +13,7 @@ class AutoReduction():
   def __init__(self, nexus_file, output_directory):
     print nexus_file, output_directory
     self._nexus_file = nexus_file
-    self._output_directory = output_directory
-    print nexus_file  
+    self._output_directory = output_directory 
 
   def E2V(self):
      # for energy in mev returns velocity in m/s
@@ -112,70 +110,81 @@ class AutoReduction():
 
 
   def execute(self):
-    filename = os.path.split(self._nexus_file)[-1]
-    instrument = filename.split('_')[0]
-    run_number = os.path.splitext(os.path.splitext(filename.split('_')[1])[0])[0]
-    out_prefix = instrument + "_" + run_number
-    
-    config['default.facility'] = "SNS"
-    autows = "__auto_ws"
-    
-    processed_filename1 = os.path.join(self._output_directory, "msk_tube/" + out_prefix + "_msk_tube_spe.nxs")
-    nxspe_filename1=os.path.join(self._output_directory, "msk_tube/" + out_prefix + "_msk_tube.nxspe")
-    print(nxspe_filename1)
+    try:
+      filename = os.path.split(self._nexus_file)[-1]
+      instrument = filename.split('_')[0]
+      run_number = os.path.splitext(os.path.splitext(filename.split('_')[1])[0])[0]
+      out_prefix = instrument + "_" + run_number
+      self._out_prefix = out_prefix
+      
+      config['default.facility'] = "SNS"
+      autows = "__auto_ws"
+      
+      processed_filename1 = os.path.join(self._output_directory, "msk_tube/" + out_prefix + "_msk_tube_spe.nxs")
+      nxspe_filename1=os.path.join(self._output_directory, "msk_tube/" + out_prefix + "_msk_tube.nxspe")
+      print(nxspe_filename1)
 
-    processed_filename3 = os.path.join(self._output_directory, "4pixel/" + out_prefix + "_4pixel_spe.nxs")
-    nxspe_filename3=os.path.join(self._output_directory, "4pixel/" + out_prefix + "_4pixel.nxspe")
+      processed_filename3 = os.path.join(self._output_directory, "4pixel/" + out_prefix + "_4pixel_spe.nxs")
+      nxspe_filename3=os.path.join(self._output_directory, "4pixel/" + out_prefix + "_4pixel.nxspe")
+      
+      # Load the data
+      LoadEventNexus(Filename=self._nexus_file, OutputWorkspace=autows)
+      
+      # Get Ei
+      run = mtd[autows].getRun()
+      if not run.hasProperty('EnergyRequest'):
+        raise ValueError("EnergyRequest was not found")
+      
+      Ei = run['EnergyRequest'].getStatistics().mean
+      self._Ei = Ei
+      
+      # Get Angle
+      if not run.hasProperty('s1'):
+        raise ValueError("s1 was not found")
+      
+      s1 = run['s1'].getStatistics().mean
+      
+      # Work out some energy bins
+      emin = -(2.0 * Ei)
+      emax = Ei * 0.9
+      estep = 0.1
+      energy_bins = "%f,%f,%f" % (emin, estep, emax)
     
-    # Load the data
-    LoadEventNexus(Filename=self._nexus_file, OutputWorkspace=autows)
-    
-    # Get Ei
-    run = mtd[autows].getRun()
-    if not run.hasProperty('EnergyRequest'):
-      f = open(self._output_directory + out_prefix + ".error", 'w')
-      f.write("EnergyRequest was not found")
-      f.close()
-      raise ValueError("EnergyRequest was not found")
-    
-    Ei = run['EnergyRequest'].getStatistics().mean
-    self._Ei = Ei
-    
-    # Get Angle
-    if not run.hasProperty('s1'):
-      f = open(self._output_directory + out_prefix + ".error", 'w')
-      f.write("s1 was not found")
-      f.close()
-      raise ValueError("s1 was not found")
-    
-    s1 = run['s1'].getStatistics().mean
-    
-    # Work out some energy bins
-    emin = -(2.0 * Ei)
-    emax = Ei * 0.9
-    estep = 0.1
-    energy_bins = "%f,%f,%f" % (emin, estep, emax)
-  
-    #TIB limits
-    tib = self.SpurionPromptPulse2()
-    
-    #reduction command
-    DgsReduction(SampleInputWorkspace=autows, IncidentEnergyGuess=Ei, EnergyTransferRange=energy_bins,
+      #TIB limits
+      tib = self.SpurionPromptPulse2()
+      
+      #reduction command
+      DgsReduction(SampleInputWorkspace=autows, IncidentEnergyGuess=Ei, EnergyTransferRange=energy_bins,
 		GroupingFile='/SNS/HYSA/shared/autoreduce/128x1pixels.xml', IncidentBeamNormalisation='ByCurrent', HardMaskFile='/SNS/HYSA/shared/autoreduce/MonsterMask.xml',
-            TimeIndepBackgroundSub='1', TibTofRangeStart=tib[0], TibTofRangeEnd=tib[1], OutputWorkspace="out1")
-    
-    DgsReduction(SampleInputWorkspace=autows,IncidentEnergyGuess=Ei,EnergyTransferRange=energy_bins,
+              TimeIndepBackgroundSub='1', TibTofRangeStart=tib[0], TibTofRangeEnd=tib[1], OutputWorkspace="out1")
+      
+      DgsReduction(SampleInputWorkspace=autows,IncidentEnergyGuess=Ei,EnergyTransferRange=energy_bins,
 		GroupingFile='/SNS/HYSA/shared/autoreduce/4x1pixels.xml',       
-    IncidentBeamNormalisation='ByCurrent',
-              HardMaskFile='/SNS/HYSA/shared/autoreduce/TubeTipMask.xml',
+      IncidentBeamNormalisation='ByCurrent',
+                HardMaskFile='/SNS/HYSA/shared/autoreduce/TubeTipMask.xml',
 		TimeIndepBackgroundSub='1',TibTofRangeStart=tib[0],TibTofRangeEnd=tib[1],OutputWorkspace="out3")
 
-    # Save files
-    SaveNexus(Filename=processed_filename1, InputWorkspace="out1")
-    SaveNXSPE(Filename=nxspe_filename1, InputWorkspace="out1", Psi=str(s1), KiOverKfScaling='1') 
+      # Save files
+      SaveNexus(Filename=processed_filename1, InputWorkspace="out1")
+      SaveNXSPE(Filename=nxspe_filename1, InputWorkspace="out1", Psi=str(s1), KiOverKfScaling='1') 
 
-    SaveNexus(Filename=processed_filename3, InputWorkspace="out3")
-    SaveNXSPE(Filename=nxspe_filename3, InputWorkspace="out3", Psi=str(s1), KiOverKfScaling='1')
+      SaveNexus(Filename=processed_filename3, InputWorkspace="out3")
+      SaveNXSPE(Filename=nxspe_filename3, InputWorkspace="out3", Psi=str(s1), KiOverKfScaling='1')
+      
+    except RuntimeError, e:
+      self.writeError()
+      raise e
+    except KeyError, e:
+      self.writeError()
+      raise e                    
+    except Exception, e:
+      self.writeError()
+      raise e
+        
+  def writeError(self):
+    f = open(self._output_directory + self._out_prefix + ".error", 'w')
+    f.write(traceback.format_exc())
+    f.close()
 
 
 
