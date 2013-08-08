a4e53f9ac934b8d9a8f2c343d0b8c1d636fe7ceb
Shelly Ren <rens@ornl.gov>
2013-08-08 19:59:36 -0400

    count running processes, and spawn new sub processes when counts are low

--- SNSPostProcessRPM/rpmbuild/autoreduce-mq/usr/bin/PostProcess.py
+++ SNSPostProcessRPM/rpmbuild/autoreduce-mq/usr/bin/PostProcess.py
@@ -110,13 +110,13 @@ class PostProcess:
         try:         
             self.send('/queue/'+self.conf.reduction_started, json.dumps(self.data))  
             logging.info("called /queue/" + self.conf.reduction_started + " --- " + json.dumps(self.data))  
-            #instrument_shared_dir = "/" + self.facility + "/" + self.instrument + "/shared/autoreduce/"
-            instrument_shared_dir = "/tmp/shelly2/"
+            instrument_shared_dir = "/" + self.facility + "/" + self.instrument + "/shared/autoreduce/"
+            #instrument_shared_dir = "/tmp/shelly2/"
             reduce_script = "reduce_" + self.instrument
             reduce_script_path = instrument_shared_dir + reduce_script  + ".py"
             
-            #proposal_shared_dir = "/" + self.facility + "/" + self.instrument + "/" + self.proposal + "/shared/autoreduce/"
-            proposal_shared_dir = "/tmp/shelly2/"
+            proposal_shared_dir = "/" + self.facility + "/" + self.instrument + "/" + self.proposal + "/shared/autoreduce/"
+            #proposal_shared_dir = "/tmp/shelly2/"
             log_dir = proposal_shared_dir + "reduction_log/"
 
             if not os.path.exists(log_dir):
--- SNSPostProcessRPM/rpmbuild/autoreduce-mq/usr/bin/queueProcessor.py
+++ SNSPostProcessRPM/rpmbuild/autoreduce-mq/usr/bin/queueProcessor.py
@@ -2,18 +2,48 @@
 """
 ActiveMQ client for Post Process
 """
-import os, sys, subprocess, psutil, time
+import os, sys, subprocess, psutil, time, logging
 
 from PostProcess import PostProcess 
 from queueListener import Listener, Client,Configuration
 
+class StreamToLogger(object):
+    #Fake file-like stream object that redirects writes to a logger instance.
+    def __init__(self, logger, log_level=logging.INFO):
+        self.logger = logger
+        self.log_level = log_level
+        self.linebuf = ''
+ 
+    def write(self, buf):
+        for line in buf.rstrip().splitlines():
+            self.logger.log(self.log_level, line.rstrip())
+        
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(name)s %(process)d/%(threadName)s: %(message)s",
+    filename='/var/log/SNS_applications/post_process.log',
+    filemode='a'
+)
+                     
+stdout_logger = logging.getLogger('STDOUT')
+sl = StreamToLogger(stdout_logger, logging.INFO)
+sys.stdout = sl
+ 
+stderr_logger = logging.getLogger('STDERR')
+sl = StreamToLogger(stderr_logger, logging.ERROR)
+sys.stderr = sl
 
 class PostProcessListener(Listener):
     """
     ActiveMQ listener implementation for a post process client.
     """
     ## Connection used for sending messages
-    _send_connection = None
+
+    def __init__(self, conf):
+        self.proc_list = []
+        self._send_connection = None
+        self.conf = conf
+        
     
     def on_message(self, headers, message):
         """
@@ -23,27 +53,25 @@ class PostProcessListener(Listener):
         """
         
         if self._send_connection is None:
-            self._send_connection = self.configuration.get_client('post_process_listener') 
+            self._send_connection = self.conf.get_client('post_process_listener') 
 
         destination = headers["destination"]
         proc = subprocess.Popen(["python", "/usr/bin/PostProcess.py", destination, message])
+        self.proc_list.append(proc)
         
-        #while proc.poll() is None:
-        #    print "Still working"
-        #    time.sleep(1.0)
+                
+        while len(self.proc_list) > 4:
+            for i in self.proc_list:
+                if i.poll() is not None:
+                    self.proc_list.remove(i)
+            logging.info("Still working: " + str(len(self.proc_list)))
+            time.sleep(1.0)
 
         
-        
-def run():
-    """
-    Run an instance of the Post Process ActiveMQ consumer
-    """
-    # Look for configuration
-    conf = Configuration('/etc/autoreduce/post_process_consumer.conf')
-    c = Client(conf.brokers, conf.amq_user, conf.amq_pwd,
-               conf.queues, "post_process_consumer")
-    c.set_listener(PostProcessListener(conf))
-    c.listen_and_wait(0.1)
 
 if __name__ == "__main__":
-    run()
+    conf = Configuration('/etc/autoreduce/post_process_consumer.conf')
+    ppListener = PostProcessListener(conf)
+    c = Client(conf.brokers, conf.amq_user, conf.amq_pwd, conf.queues, "post_process_consumer")
+    c.set_listener(ppListener)
+    c.listen_and_wait(0.1)
