4b5ec83b219ea333c71a7cb129334189ac8daa5e
Shelly Ren <rens@ornl.gov>
2013-09-23 11:30:14 -0400

    Updated reduction script by cron job

--- SNSReductionScripts/snap/reduce_SNAP.py
+++ SNSReductionScripts/snap/reduce_SNAP.py
@@ -20,27 +20,16 @@ van_64 = LoadNexusProcessed(Filename=r'/SNS/SNAP/IPTS-9084/shared/backgrounds/SN
 
 binning='0.4,-0.002,10'
 
-
-
-
-def make_grp(wrk_spc_cols, low_d_mod,high_d_mod):
-
-	low_d = SumSpectra(InputWorkspace = wrk_spc_cols,StartWorkspaceIndex = '0',EndWorkspaceIndex=str(low_d_mod-1)) 
-	high_d = SumSpectra(InputWorkspace = wrk_spc_cols,StartWorkspaceIndex = str(low_d_mod),EndWorkspaceIndex=str(low_d_mod+1)) 
-	output = ConjoinWorkspaces(low_d,high_d)
-#	RenameWorkspace(InputWorkspace='low_d', OutputWorkspace=wrk_spc_cols.replace('_col','_grp'))
-	
-	return output
-
-
-
-
 iws=LoadEventNexus(Filename=nexus_file)
 ows=NormaliseByCurrent(iws)
 MaskDetectors(Workspace=ows, MaskedWorkspace= 'mask_edges' )
-ows=ConvertUnits(InputWorkspace=ows,Target='dSpacing',AlignBins='1')
-ows=DiffractionFocussing(InputWorkspace=ows,GroupingWorkspace='column',PreserveEvents='0')
-ows=Rebin(InputWorkspace=ows,Params=binning,PreserveEvents='0')
-ows = make_grp (ows, 4,2)
-# ows=Divide(LHSWorkspace=ows,RHSWorkspace = van_2)
-SaveAscii(InputWorkspace=ows, Filename= outputDir+out_prefix+'.dat')
+ows=ConvertUnits(InputWorkspace='ows',Target='dSpacing',AlignBins='1')
+ows=DiffractionFocussing(InputWorkspace='ows',GroupingWorkspace='column',PreserveEvents='1')
+ows=Rebin(InputWorkspace='ows',Params=binning,PreserveEvents='0')
+SumSpectra(InputWorkspace = ows,OutputWorkspace = 'low_d', StartWorkspaceIndex = '0',EndWorkspaceIndex='3') 
+SumSpectra(InputWorkspace = ows,OutputWorkspace = 'high_d',StartWorkspaceIndex = '4',EndWorkspaceIndex='5') 
+ConjoinWorkspaces(InputWorkspace1='low_d',InputWorkspace2='high_d')
+RenameWorkspace(InputWorkspace='low_d', OutputWorkspace='ows')
+ows=Divide(LHSWorkspace='ows',RHSWorkspace = 'van_2')
+SaveNexusProcessed(InputWorkspace='ows', Title='Conjoined', Filename = outputDir+'conjoined.nxs')
+
