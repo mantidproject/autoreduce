70936ae412b3f75c1cce0552941a7436d6530fb7
Shelly Ren <rens@ornl.gov>
2013-10-14 10:40:02 -0400

    Updated reduction script by cron job

--- SNSReductionScripts/arcs/reduce_ARCS.py
+++ SNSReductionScripts/arcs/reduce_ARCS.py
@@ -147,10 +147,7 @@ if __name__ == "__main__":
         ylabel('E (meV)')
         title("Run "+outfile)
 
-        dir="/SNS/ARCS/IPTS-9168/shared/autoreduce/ARCS_39798_autoreduced.nxs.png"
-
-        savefig(dir,bbox_inches='tight')
-        #savefig(str(outdir+outfile+".nxs.png"),bbox_inches='tight')
+        savefig(str(outdir+outfile+".nxs.png"),bbox_inches='tight')
 
         if NXSPE_flag:            
             SaveNXSPE(InputWorkspace="__OWS", Filename= outdir+outfile+".nxspe",Efixed=Ei,Psi=angle,KiOverKfScaling=True) 
