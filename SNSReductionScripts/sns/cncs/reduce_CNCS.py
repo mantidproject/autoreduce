25a37315ba04c446e0e1aeb746bd017553edbcb6
Shelly Ren <rens@ornl.gov>
2013-10-24 12:15:01 -0400

    Updated reduction script by cron job

--- SNSReductionScripts/sns/cncs/reduce_CNCS.py
+++ SNSReductionScripts/sns/cncs/reduce_CNCS.py
@@ -1,7 +1,17 @@
+#!/usr/bin/env python
+
+import os, sys, traceback
+sys.path.insert(0,"/mnt/software/lib/python2.6/site-packages/matplotlib-1.2.0-py2.6-linux-x86_64.egg/")
+from string import *
+from numpy import *
+
 import sys,os
 sys.path.append("/opt/Mantid/bin")
 from mantid.simpleapi import *
-from string import *
+
+from matplotlib import *
+use("agg")
+from matplotlib.pyplot import *
 
 config['default.facility']="SNS"
 nexus_file=sys.argv[1]
@@ -39,3 +49,31 @@ s1=mtd["reduce"].getRun()['SERotator2'].value[0]
 # Save a file
 SaveNexus(Filename=processed_filename, InputWorkspace="reduce")
 SaveNXSPE(Filename=nxspe_filename, InputWorkspace="reduce", Psi=str(s1), KiOverKfScaling='1')
+
+
+minvals,maxvals=ConvertToMDHelper('reduce','|Q|','Direct')
+xmin=minvals[0]
+xmax=maxvals[0]
+xstep=(xmax-xmin)*0.01
+ymin=minvals[1]
+ymax=maxvals[1]
+ystep=(ymax-ymin)*0.01
+x=arange(xmin,xmax,xstep)
+y=arange(ymin,ymax,ystep)
+X,Y=meshgrid(x,y)
+
+
+MD=ConvertToMD('reduce',QDimensions='|Q|',dEAnalysisMode='Direct',MinValues=minvals,MaxValues=maxvals)
+ad0='|Q|,'+str(xmin)+','+str(xmax)+',100'
+ad1='DeltaE,'+str(ymin)+','+str(ymax)+',100'
+MDH=BinMD(InputWorkspace=MD,AlignedDim0=ad0,AlignedDim1=ad1)
+d=MDH.getSignalArray()
+ne=MDH.getNumEventsArray()
+dne=d/ne
+
+Zm=ma.masked_where(ne==0,dne)
+pcolormesh(X,Y,log(Zm),shading='gouraud')
+xlabel('|Q| ($\AA^{-1}$)')
+ylabel('E (meV)')
+savefig(processed_filename+'.png',bbox_inches='tight')
+
