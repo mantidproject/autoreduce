c34624933022d2d82844c75194a6113759170250
Shelly Ren <rens@ornl.gov>
2013-11-20 17:20:03 -0500

    Updated reduction script by cron job

--- ReductionScripts/sns/sequoia/reduce_SEQ.py
+++ ReductionScripts/sns/sequoia/reduce_SEQ.py
@@ -55,7 +55,7 @@ def WS_clean():
 if __name__ == "__main__":
 
     #processing parameters
-    RawVanadium="/SNS/SEQ/shared/2013_B/V_files/SEQ_43316.nxs.h5"
+    RawVanadium="/SNS/SEQ/shared/2013_B/V_files/SEQ_43817.nxs.h5"
     ProcessedVanadium='van.nxs'
     HardMaskFile=''
     IntegrationRange=[0.3,1.2] #integration range for Vanadium in angstroms
