d8c673a76712b3d359e512152b7f449473ca55c6
Shelly Ren <rens@ornl.gov>
2013-11-20 10:00:04 -0500

    Updated reduction script by cron job

--- ReductionScripts/sns/sequoia/reduce_SEQ.py
+++ ReductionScripts/sns/sequoia/reduce_SEQ.py
@@ -63,6 +63,14 @@ if __name__ == "__main__":
     MaskBTPParameters.append({'Bank':"99,100,101,102,118"})
     MaskBTPParameters.append({'Bank':"74",'Tube':"8"})
     MaskBTPParameters.append({'Bank':"127",'Tube':"8"})
+    
+    #Added these masked pixels for HOT spots on detector
+    #maskBTPParameters.append({'bank':"127" tube='8' pixel='99-110'/>
+    MaskBTPParameters.append({'Bank':"88",'Tube':"3",'Pixel':"32-36"})/>
+    MaskBTPParameters.append({'Bank':"105",'Tube':"5",'Pixel':"89-91"})/>
+    MaskBTPParameters.append({'Bank':"46",'Tube':"7",'Pixel':"107-109"})/>
+    
+    
     clean=True
     NXSPE_flag=True
 
