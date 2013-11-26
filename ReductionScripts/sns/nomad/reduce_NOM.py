0cbdaa02826ea56436d98db0347be8a06365179b
Shelly Ren <rens@ornl.gov>
2013-11-26 11:35:05 -0500

    Updated reduction script by cron job

---
+++ ReductionScripts/sns/nomad/reduce_NOM.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import shutil 
+sys.path.append("/opt/mantidnightly/bin")
+from mantid.simpleapi import *
+import mantid
+
+cal_dir = "/SNS/NOM/IPTS-9111/shared"
+cal_file  = os.path.join(cal_dir, "autoreduce", "NOM_calibrate_d22213_2013_11_26.cal")
+char_file = "/SNS/NOM/shared/NOM_characterizations.txt" #os.path.join(cal_dir, "NOM_characterizations.txt")
+sam_back =     22229
+van      =     22215
+van_back =     22216
+
+#from mantidsimple import *
+
+eventFileAbs=sys.argv[1]
+outputDir=sys.argv[2]
+maxChunkSize=0.
+if len(sys.argv)>3:
+    maxChunkSize=float(sys.argv[3])
+
+eventFile = os.path.split(eventFileAbs)[-1]
+nexusDir = eventFileAbs.replace(eventFile, '')
+runNumber = eventFile.split('_')[1]
+configService = mantid.config
+dataSearchPath = configService.getDataSearchDirs()
+dataSearchPath.append(nexusDir)
+configService.setDataSearchDirs(";".join(dataSearchPath))
+
+SNSPowderReduction(Instrument="NOM", RunNumber=runNumber, Extension="_event.nxs",
+                   MaxChunkSize=maxChunkSize, PreserveEvents=True,PushDataPositive='AddMinimum',
+                   CalibrationFile=cal_file, CharacterizationRunsFile=char_file,
+                   BackgroundNumber=sam_back, VanadiumNumber=van,
+                   VanadiumBackgroundNumber=van_back, RemovePromptPulseWidth=50,
+                   ResampleX=-3000, BinInDspace=True, FilterBadPulses=True,
+                   SaveAs="gsas and fullprof and pdfgetn", OutputDirectory=outputDir,
+                   StripVanadiumPeaks=True,
+                   NormalizeByCurrent=True, FinalDataUnits="MomentumTransfer")
