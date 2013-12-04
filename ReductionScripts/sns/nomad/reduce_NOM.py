456e2b127c488d03c43e728de49719b5c05f379b
Shelly Ren <rens@ornl.gov>
2013-12-04 12:00:06 -0500

    Updated reduction script by cron job

--- ReductionScripts/sns/nomad/reduce_NOM.py
+++ ReductionScripts/sns/nomad/reduce_NOM.py
@@ -1,15 +1,11 @@
 import os
-
 import sys
-
 import shutil 
 
 
 
 sys.path.append("/opt/mantidnightly/bin")
-
 from mantid.simpleapi import *
-
 import mantid
 
 
@@ -17,22 +13,12 @@ import mantid
 
 
 cal_dir = "/SNS/NOM/IPTS-10924/shared"
-
 cal_file  = os.path.join(cal_dir, "NOM_calibrate_d22523_2013_12_04.cal")
-
 char_file = "/SNS/NOM/shared/NOM_characterizations.txt" #os.path.join(cal_dir, "NOM_characterizations.txt")
 
 sam_back =     22527
-
 van      =     22525
-
 van_back =     22526
-
-
-
-
-
-
 
 #from mantidsimple import *
 
@@ -43,15 +29,12 @@ van_back =     22526
 
 
 eventFileAbs=sys.argv[1]
-
 outputDir=sys.argv[2]
-
 maxChunkSize=0.
 
 
 
 if len(sys.argv)>3:
-
     maxChunkSize=float(sys.argv[3])
 
 
@@ -61,17 +44,12 @@ if len(sys.argv)>3:
 
 
 eventFile = os.path.split(eventFileAbs)[-1]
-
 nexusDir = eventFileAbs.replace(eventFile, '')
-
 runNumber = eventFile.split('_')[1]
 
 configService = mantid.config
-
 dataSearchPath = configService.getDataSearchDirs()
-
 dataSearchPath.append(nexusDir)
-
 configService.setDataSearchDirs(";".join(dataSearchPath))
 
 
@@ -79,20 +57,11 @@ configService.setDataSearchDirs(";".join(dataSearchPath))
 
 
 SNSPowderReduction(Instrument="NOM", RunNumber=runNumber, Extension="_event.nxs",
-
                    MaxChunkSize=maxChunkSize, PreserveEvents=True,PushDataPositive='AddMinimum',
-
                    CalibrationFile=cal_file, CharacterizationRunsFile=char_file,
-
                    BackgroundNumber=sam_back, VanadiumNumber=van,
-
                    VanadiumBackgroundNumber=van_back, RemovePromptPulseWidth=50,
-
                    ResampleX=-3000, BinInDspace=True, FilterBadPulses=True,
-
                    SaveAs="gsas and fullprof and pdfgetn", OutputDirectory=outputDir,
-
                    StripVanadiumPeaks=True,
-
                    NormalizeByCurrent=True, FinalDataUnits="MomentumTransfer")
-
