25eb2edf908083ac5db526ec28d172b4b8727e03
Shelly Ren <rens@ornl.gov>
2013-12-14 13:50:06 -0500

    Updated reduction script by cron job

--- ReductionScripts/sns/cncs/reduce_CNCS.py
+++ ReductionScripts/sns/cncs/reduce_CNCS.py
@@ -19,9 +19,9 @@ DgsReduction(
              EnergyTransferRange=erange,
              IncidentBeamNormalisation="ByCurrent",
              TimeIndepBackgroundSub=True,
-             TibTofRangeStart=23000.0,
-             TibTofRangeEnd=25000.0,
-             DetectorVanadiumInputFile="/SNS/CNCS/shared/Va_from_IPTS-7477/CNCS_57794_event.nxs",
+             TibTofRangeStart=12000.0,
+             TibTofRangeEnd=16500.0,
+             DetectorVanadiumInputFile="/SNS/CNCS/IPTS-4654/18/CNCS_71909_event.nxs",
              UseBoundsForDetVan=True,
              DetVanIntRangeLow=52000.0,
              DetVanIntRangeHigh=53000.0,
@@ -34,7 +34,7 @@ run_number = os.path.splitext(os.path.splitext(filename.split('_')[1])[0])[0]
 processed_filename = os.path.join(output_directory, "CNCS_" + run_number + "_spe.nxs")
 nxspe_filename=os.path.join(output_directory, "CNCS_" + run_number + ".nxspe")
 # Get Angle
-s1=mtd["reduce"].getRun()['SERotator2'].value[0]
+s1=mtd["reduce"].getRun()['SEHOT09'].value[0]
 # Save a file
 SaveNexus(Filename=processed_filename, InputWorkspace="reduce")
 SaveNXSPE(Filename=nxspe_filename, InputWorkspace="reduce", Psi=str(s1), KiOverKfScaling='1')
