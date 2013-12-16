5b260d91acbb079ea1601d682cdc26b92f91b748
Shelly Ren <rens@ornl.gov>
2013-12-16 13:30:08 -0500

    Updated reduction script by cron job

--- ReductionScripts/sns/cncs/reduce_CNCS.py
+++ ReductionScripts/sns/cncs/reduce_CNCS.py
@@ -1,7 +1,17 @@
+#!/usr/bin/env python
+
+import os, sys, traceback
+sys.path.insert(0,"/mnt/software/lib/python2.6/site-packages/matplotlib-1.2.0-py2.6-linux-x86_64.egg/")
+from string import *
+from numpy import *
+
 import sys,os
 sys.path.append("/opt/Mantid/bin")
 from mantid.simpleapi import *
-from string import *
+
+from matplotlib import *
+use("agg")
+from matplotlib.pyplot import *
 
 config['default.facility']="SNS"
 nexus_file=sys.argv[1]
@@ -10,6 +20,7 @@ output_directory=sys.argv[2]
 w=Load(nexus_file)
 Ei=w.getRun()['EnergyRequest'].firstValue()
 erange=str(-Ei)+','+str(0.01*Ei)+','+str(0.95*Ei)
+tib=SuggestTibCNCS(Ei)
 
 DgsReduction(
              SampleInputFile=nexus_file,
@@ -19,8 +30,8 @@ DgsReduction(
              EnergyTransferRange=erange,
              IncidentBeamNormalisation="ByCurrent",
              TimeIndepBackgroundSub=True,
-#             TibTofRangeStart=12000.0,
-#             TibTofRangeEnd=16500.0,
+             TibTofRangeStart=tib[0],
+             TibTofRangeEnd=tib[1],
              DetectorVanadiumInputFile="/SNS/CNCS/IPTS-4654/18/71909/NeXus/CNCS_71909_event.nxs",
              UseBoundsForDetVan=True,
              DetVanIntRangeLow=52000.0,
@@ -42,3 +53,31 @@ nxspe_filename=os.path.join(output_directory, "CNCS_" + run_number + ".nxspe")
 # Save a file
 SaveNexus(Filename=processed_filename, InputWorkspace="reduce")
 SaveNXSPE(Filename=nxspe_filename, InputWorkspace="reduce", Psi=str(s1), KiOverKfScaling='1')
+
+# make a pretty image
+minvals,maxvals=ConvertToMDHelper('reduce','|Q|','Direct')
+xmin=minvals[0]
+xmax=maxvals[0]
+xstep=(xmax-xmin)*0.01
+ymin=minvals[1]
+ymax=maxvals[1]
+ystep=(ymax-ymin)*0.01
+x=arange(xmin,xmax,xstep)
+y=arange(ymin,ymax,ystep)
+X,Y=meshgrid(x,y)
+
+
+MD=ConvertToMD('reduce',QDimensions='|Q|',dEAnalysisMode='Direct',MinValues=minvals,MaxValues=maxvals)
+ad0='|Q|,'+str(xmin)+','+str(xmax)+',100'
+ad1='DeltaE,'+str(ymin)+','+str(ymax)+',100'
+MDH=BinMD(InputWorkspace=MD,AlignedDim0=ad0,AlignedDim1=ad1)
+d=MDH.getSignalArray()
+ne=MDH.getNumEventsArray()
+dne=d/ne
+
+Zm=ma.masked_where(ne==0,dne)
+pcolormesh(X,Y,log(Zm),shading='gouraud')
+xlabel('|Q| ($\AA^{-1}$)')
+ylabel('E (meV)')
+savefig(processed_filename+'.png',bbox_inches='tight')
+
