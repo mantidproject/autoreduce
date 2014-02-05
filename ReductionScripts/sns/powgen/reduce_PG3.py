8e8446ad5fc817977b3e56805af1307a12c77d72
Shelly Ren <rens@ornl.gov>
2014-02-05 14:00:03 -0500

    Updated reduction script by cron job

--- ReductionScripts/sns/powgen/reduce_PG3.py
+++ ReductionScripts/sns/powgen/reduce_PG3.py
@@ -28,7 +28,7 @@ SNSPowderReduction(Instrument="PG3", RunNumber=runNumber, Extension="_event.nxs"
                    PreserveEvents=True,PushDataPositive="None",
                    CalibrationFile=cal_file, CharacterizationRunsFile=char_file,
                    LowResRef=0, RemovePromptPulseWidth=50,
-                   Binning=-0.0008, BinInDspace=True, FilterBadPulses=True,
+                   Binning=-0.0008, BinInDspace=True, FilterBadPulses=False,
                    SaveAs="gsas and fullprof", OutputDirectory=outputDir,
                    NormalizeByCurrent=True, FinalDataUnits="dSpacing")
 
