d3c1510e0739fb33ee22bcb132ab68c95522e016
mantid-publisher <mantid-developers@mantidproject.org>
2014-05-29 09:31:03 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/powgen/reduce_PG3.py
+++ ReductionScripts/sns/powgen/reduce_PG3.py
@@ -1,23 +1,16 @@
 import os
 import sys
 import shutil 
-import numpy
-#sys.path.append("/opt/Mantid/bin")
 sys.path.append("/opt/mantidnightly/bin")
-sys.path.insert(0,"/mnt/software/lib/python2.6/site-packages/matplotlib-1.2.0-py2.6-linux-x86_64.egg/")
 from mantid.simpleapi import *
 import mantid
 from matplotlib import *
-use("agg")
-from matplotlib.pyplot import *
 cal_dir = "/SNS/PG3/2014_1_11A_CAL/"
 cal_file  = os.path.join(cal_dir, "PG3_PAC_d17532_2014_02_14.cal")
 char_file = os.path.join(cal_dir, "PG3_characterization_2014_02_11-HR-PAC-BGsub.txt")
 #MODE = 0664
 
 #from mantidsimple import *
-
-numpy.seterr("ignore") #ignore division by 0 warning in plots
 
 eventFileAbs=sys.argv[1]
 outputDir=sys.argv[2]
@@ -39,15 +32,7 @@ SNSPowderReduction(Instrument="PG3", RunNumber=runNumber, Extension="_event.nxs"
                    SaveAs="gsas topas and fullprof", OutputDirectory=outputDir,
                    FinalDataUnits="dSpacing")
 
-s=mtd["PG3_"+runNumber]
-x=s.readX(0)
-y=s.readY(0)
-plot(x[1:],y)
-xlabel('d($\AA$)')
-ylabel('Intensity')
-#yscale('log')
-show()
-savefig(outputDir+"PG3_"+runNumber+'.png',bbox_inches='tight')
+SavePlot1D(InputWorkspace="PG3_"+runNumber, OutputFilename=outputDir+"PG3_"+runNumber+'.png',  YLabel='Intensity')
 
 #dirList=os.listdir(outputDir)
 #for fname in dirList:
