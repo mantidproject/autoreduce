912e1e62e2f988247c3a5c109ace782393bf5e86
mantid-publisher <mantid-developers@mantidproject.org>
2014-06-11 14:26:03 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/cncs/reduce_CNCS.py
+++ ReductionScripts/sns/cncs/reduce_CNCS.py
@@ -24,9 +24,7 @@ w=Load(nexus_file)
 Ei=w.getRun()['EnergyRequest'].firstValue()
 erange=str(-Ei*0.2)+','+str(0.01*Ei)+','+str(0.9*Ei)
 
-#tib=SuggestTibCNCS(Ei)
-tib[0]=43000.0
-tib[1]=44000.0
+tib=SuggestTibCNCS(Ei)
 
 DgsReduction(
              SampleInputFile=nexus_file,
