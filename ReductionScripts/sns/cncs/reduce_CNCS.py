35bf46c994419f6ae11766b792dfaeed6d0d2e9c
mantid-publisher <mantid-developers@mantidproject.org>
2014-08-22 10:16:04 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/cncs/reduce_CNCS.py
+++ ReductionScripts/sns/cncs/reduce_CNCS.py
@@ -29,7 +29,7 @@ tib=SuggestTibCNCS(Ei)
 DgsReduction(
              SampleInputFile=nexus_file,
              OutputWorkspace="reduce",
-             HardMaskFile="/SNS/CNCS/shared/autoreduce/mask8.xml",
+             HardMaskFile="/SNS/CNCS/shared/autoreduce/mask8bothsides.xml",
              GroupingFile='/SNS/CNCS/shared/autoreduce/CNCS_2x1.xml',
              EnergyTransferRange=erange,
              IncidentBeamNormalisation="ByCurrent",
@@ -50,7 +50,7 @@ filename = os.path.split(nexus_file)[-1]
 elog=ExperimentLog()
 elog.setLogList('Speed1,Phase1,Speed2,Phase2,Speed3,Phase3,Speed4,Phase4,Speed5,Phase5,EnergyRequest')
 elog.setSimpleLogList("EnergyRequest")
-elog.setSERotOptions('SERotator2,huber')
+elog.setSERotOptions('SERotator2')
 #elog.setSERotOptions('SERotator2,OxDilRot,CCR13VRot,FatSamVRot,SEOCRot,huber,CCR10G2Rot')
 elog.setSETempOptions('SampleTemp,sampletemp,SensorC,SensorB,SensorA')
 elog.setFilename(output_directory+'experiment_log.csv')
