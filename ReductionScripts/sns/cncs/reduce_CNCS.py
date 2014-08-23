6464cd9ff2ea5ba5be90ab7fda4b9b1218f860fc
mantid-publisher <mantid-developers@mantidproject.org>
2014-08-23 12:16:04 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/cncs/reduce_CNCS.py
+++ ReductionScripts/sns/cncs/reduce_CNCS.py
@@ -29,8 +29,8 @@ tib=SuggestTibCNCS(Ei)
 DgsReduction(
              SampleInputFile=nexus_file,
              OutputWorkspace="reduce",
-             HardMaskFile="/SNS/CNCS/shared/autoreduce/mask8bothsides.xml",
-             GroupingFile='/SNS/CNCS/shared/autoreduce/CNCS_2x1.xml',
+             HardMaskFile="/SNS/CNCS/shared/autoreduce/mask8.xml",
+             GroupingFile='/SNS/CNCS/shared/autoreduce/CNCS_4x2.xml',
              EnergyTransferRange=erange,
              IncidentBeamNormalisation="ByCurrent",
              TimeIndepBackgroundSub=True,
