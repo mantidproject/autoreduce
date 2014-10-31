7bd2655add7c55bc5f437ece4aaa8dcc0f441dc4
mantid-publisher <mantid-developers@mantidproject.org>
2014-10-31 16:05:07 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/sequoia/reduce_SEQ.py
+++ ReductionScripts/sns/sequoia/reduce_SEQ.py
@@ -123,8 +123,8 @@ def WS_clean():
 if __name__ == "__main__":
     numpy.seterr("ignore")#ignore division by 0 warning in plots
     #processing parameters
-    RawVanadium="/SNS/SEQ/IPTS-11831/nexus/SEQ_58383.nxs.h5"
-    ProcessedVanadium="van58383.nxs"
+    RawVanadium="/SNS/SEQ/IPTS-12246/nexus/SEQ_60992.nxs.h5"
+    ProcessedVanadium="van60992.nxs"
     HardMaskFile=''
     IntegrationRange=[0.3,1.2] #integration range for Vanadium in angstroms
     MaskBTPParameters=[{'Pixel':"1-8,121-128"}]
@@ -137,8 +137,8 @@ if __name__ == "__main__":
     #MaskBTPParameters.append({'Bank':"141"})
     #MaskBTPParameters.append({'Bank':"70"})
     MaskBTPParameters.append({'Pixel': '1-8,121-128'})
-    MaskBTPParameters.append({'Bank': '98-115,137-150'})
-    MaskBTPParameters.append({'Bank': '61-76,38-39'})
+    MaskBTPParameters.append({'Bank': '99-102,114,115,141'})
+    MaskBTPParameters.append({'Bank': '70,75-76,38-39'})
     MaskBTPParameters.append({'Tube': '8', 'Bank': '74'})
     MaskBTPParameters.append({'Tube': '8', 'Pixel': '104-114', 'Bank': '127'})
     MaskBTPParameters.append({'Tube': '3', 'Pixel': '32-36', 'Bank': '88'})
@@ -185,7 +185,7 @@ if __name__ == "__main__":
         DGSdict['IncidentEnergyGuess']=Ei
         DGSdict['UseIncidentEnergyGuess']='1'
         DGSdict['TimeZeroGuess']=T0
-        DGSdict['EnergyTransferRange']=[-0.9*EGuess,0.005*EGuess,0.89*EGuess]  #Typical values are -0.5*EGuess, 0.005*EGuess, 0.95*EGuess
+        DGSdict['EnergyTransferRange']=[-0.95*EGuess,0.005*EGuess,0.95*EGuess]  #Typical values are -0.5*EGuess, 0.005*EGuess, 0.95*EGuess
         DGSdict['SofPhiEIsDistribution']='0' # keep events
         DGSdict['HardMaskFile']=HardMaskFile
         DGSdict['GroupingFile']="/SNS/SEQ/shared/autoreduce/SEQ_1x1_grouping.xml"#'/SNS/SEQ/shared/autoreduce/SEQ_2x2_grouping.xml' #Typically an empty string '', choose 2x1 or some other grouping file created by GenerateGroupingSNSInelastic or GenerateGroupingPowder
