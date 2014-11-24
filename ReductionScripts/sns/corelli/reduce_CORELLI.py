32064b03eb0e3667e2422c441a234503aa2715f5
mantid-publisher <mantid-developers@mantidproject.org>
2014-11-24 17:41:06 -0500

    Updated reduction script by cron job

--- ReductionScripts/sns/corelli/reduce_CORELLI.py
+++ ReductionScripts/sns/corelli/reduce_CORELLI.py
@@ -7,8 +7,9 @@ from mantid.simpleapi import *
 
 nexus_file=sys.argv[1]
 output_directory=sys.argv[2]
+output_file=os.path.split(nexus_file)[-1].replace('.nxs.h5','')
 
 w=Load(nexus_file)
 LoadInstrument(w, MonitorList='-1,-2,-3', InstrumentName='CORELLI')
 cc=CorelliCrossCorrelate(w,56000)
-SaveNexus(cc, Filename=output_directory+nexus_file+"_cc.nxs")
+SaveNexus(cc, Filename=output_directory+output_file+'_cc.nxs')
