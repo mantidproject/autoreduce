9ebec0a13683cf4b0427a356395ccc701e91d3ca
mantid-publisher <mantid-developers@mantidproject.org>
2015-04-08 12:50:08 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/ref_l/reduce_REF_L.py
+++ ReductionScripts/sns/ref_l/reduce_REF_L.py
@@ -331,9 +331,9 @@ if n_ts>1:
 SaveNexus(Filename=os.path.join(outputDir,"REFL_%s_%s_%s_auto.nxs" % (first_run_of_set, sequence_number, runNumber)), InputWorkspace=output_ws)
 
 
-_create_ascii_clicked(first_run_of_set)
-#from liquids_reflectometry_stitching import autoreduction_stitching
-#autoreduction_stitching(outputDir, first_run_of_set, "auto")
+#_create_ascii_clicked(first_run_of_set)
+from liquids_reflectometry_stitching import autoreduction_stitching
+autoreduction_stitching(outputDir, first_run_of_set, "auto")
 
 result_list = ['auto']
 if compare:
