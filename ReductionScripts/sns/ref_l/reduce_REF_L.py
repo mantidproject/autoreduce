54632b0bf48c20c4e3ff09e9cc2cca9b019c6be0
mantid-publisher <mantid-developers@mantidproject.org>
2015-04-09 11:47:04 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/ref_l/reduce_REF_L.py
+++ ReductionScripts/sns/ref_l/reduce_REF_L.py
@@ -2,10 +2,18 @@ import sys
 import os
 import re
 import math
+sys.path.insert(0,"/mnt/software/lib/python2.6/site-packages/matplotlib-1.2.0-py2.6-linux-x86_64.egg/")
+sys.path.append("/opt/Mantid/bin")
+from mantid.simpleapi import *
+from matplotlib import *
+use("agg")
+import warnings
+warnings.filterwarnings('ignore',module='matplotlib')
+
 #sys.path.insert(0,"/mnt/software/lib/python2.6/site-packages/matplotlib-1.2.0-py2.6-linux-x86_64.egg/")
 #import matplotlib
 #matplotlib.use('agg', warn=False)
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 if (os.environ.has_key("MANTIDPATH")):
     del os.environ["MANTIDPATH"]
