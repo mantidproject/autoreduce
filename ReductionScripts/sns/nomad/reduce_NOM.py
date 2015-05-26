bda640e3758b377ea8e7068e729f529dca74b08a
mantid-publisher <mantid-developers@mantidproject.org>
2015-05-26 09:51:03 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/nomad/reduce_NOM.py
+++ ReductionScripts/sns/nomad/reduce_NOM.py
@@ -5,12 +5,12 @@ sys.path.append("/opt/Mantid/bin")
 from mantid.simpleapi import *
 import mantid
 
-cal_dir = "/SNS/NOM/IPTS-11504/shared"
-cal_file  = os.path.join(cal_dir, "NOM_calibrate_d35511_2015_05_21.cal")
+cal_dir = "/SNS/NOM/IPTS-14060/shared"
+cal_file  = os.path.join(cal_dir, "NOM_calibrate_d45623_2015_05_26.cal")
 char_file = "/SNS/NOM/shared/NOM_characterizations.txt" #os.path.join(cal_dir, "NOM_characterizations.txt")
-sam_back =     35084
-van      =     35513
-van_back =     35514
+sam_back =     45626
+van      =     45625
+van_back =     45626
 
 #from mantidsimple import *
 
