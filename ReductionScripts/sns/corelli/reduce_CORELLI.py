269bffd625372b772cd8803e57ba7311839c0b10
mantid-publisher <mantid-developers@mantidproject.org>
2015-06-11 16:23:03 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/corelli/reduce_CORELLI.py
+++ ReductionScripts/sns/corelli/reduce_CORELLI.py
@@ -19,7 +19,7 @@ class processInputs(object):
         self.vanadium_SA_file='/SNS/CORELLI/shared/Vanadium/SolidAngle20150601.nxs' #'/SNS/CORELLI/shared/Vanadium/SolidAngle20150411.nxs'
         self.vanadium_flux_file='/SNS/CORELLI/shared/Vanadium/Spectrum20150601.nxs' #'/SNS/CORELLI/shared/Vanadium/Spectrum20150411.nxs'
         self.mask=[] #[{'Tube':'1,2,3,4','Bank':'','Pixel':''}]
-        self.plot_requests=[{'Minimum': '-0.1', 'PerpendicularTo': '[0,K,0]', 'Maximum': '0.1'}, {'Minimum': '-0.1', 'PerpendicularTo': '[H,0,0]', 'Maximum': '0.1'}, {'Minimum': '-0.1', 'PerpendicularTo': '[0,0,L]', 'Maximum': '0.1'}] #[{'PerpendicularTo':"[0,K,0]",'Minimum':'-0.05','Maximum':'0.05'},{'PerpendicularTo':"[0,K,0]",'Minimum':'10.95','Maximum':'11.05'},{'PerpendicularTo':"[0,K,0]",'Minimum':'0.95','Maximum':'1.05'}]
+        self.plot_requests=[{'Minimum': '-0.1', 'PerpendicularTo': '[H,0,0]', 'Maximum': '0.1'}] #[{'PerpendicularTo':"[0,K,0]",'Minimum':'-0.05','Maximum':'0.05'},{'PerpendicularTo':"[0,K,0]",'Minimum':'10.95','Maximum':'11.05'},{'PerpendicularTo':"[0,K,0]",'Minimum':'0.95','Maximum':'1.05'}]
         self.useCC='True' #"True"
         #other
         self.can_do_HKL=False
