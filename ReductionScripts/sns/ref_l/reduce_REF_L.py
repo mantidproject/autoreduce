163cdaf98755279ceba81a55fc1a9c5e028fea53
mantid-publisher <mantid-developers@mantidproject.org>
2015-08-13 11:50:04 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/ref_l/reduce_REF_L.py
+++ ReductionScripts/sns/ref_l/reduce_REF_L.py
@@ -179,7 +179,7 @@ if compare:
                   SlitsWidthFlag=data_set.slits_width_flag,
                   OutputWorkspace='reflectivity_%s_%s_%s' % (first_run_of_set, sequence_number, runNumber))
 
-    is_absolute_new = save_partial_output(endswith=comparison_ending, to_file=False, scale_to_unity=True)
+    is_absolute_new = save_partial_output(endswith=comparison_ending, to_file=False, scale_to_unity=NORMALIZE_TO_UNITY)
 
     for item in AnalysisDataService.getObjectNames():
         if not item == "reflectivity_%s" % comparison_ending:
@@ -215,7 +215,7 @@ LiquidsReflectometryReduction(RunNumbers=[int(runNumber)],
               PrimaryFractionRange=PRIMARY_FRACTION_RANGE,
               OutputWorkspace='reflectivity_%s_%s_%s' % (first_run_of_set, sequence_number, runNumber))
 
-is_absolute = save_partial_output(endswith='auto', scale_to_unity=True)
+is_absolute = save_partial_output(endswith='auto', scale_to_unity=NORMALIZE_TO_UNITY)
 if AnalysisDataService.doesExist('reflectivity_auto'):
     RenameWorkspace(InputWorkspace="reflectivity_auto", OutputWorkspace="output_auto")
 
