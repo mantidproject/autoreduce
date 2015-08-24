d7f19d0b7dda4b8a088297c5a485dd16448d0dda
mantid-publisher <mantid-developers@mantidproject.org>
2015-08-24 12:21:04 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/ref_l/reduce_REF_L.py
+++ ReductionScripts/sns/ref_l/reduce_REF_L.py
@@ -31,7 +31,7 @@ from LargeScaleStructures.data_stitching import DataSet, Stitcher
 
 #-------------------------------------
 # Reduction options
-WL_CUTOFF = 9.0  # Wavelength below which we don't need the absolute normalization
+WL_CUTOFF = 10.0  # Wavelength below which we don't need the absolute normalization
 PRIMARY_FRACTION_RANGE = [118, 197] #[121,195] #[82,154]
 NORMALIZE_TO_UNITY = True
 #-------------------------------------
