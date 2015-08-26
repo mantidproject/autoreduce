55ae008d2911a31ec0f29f28e2d0c7b90a516e9a
mantid-publisher <mantid-developers@mantidproject.org>
2015-08-26 14:57:03 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/sequoia/reduce_SEQ.py
+++ ReductionScripts/sns/sequoia/reduce_SEQ.py
@@ -56,7 +56,7 @@ def preprocessData(filename):
 
     #delete all bad pulses below   10% of the average of the file.
     FilterBadPulses(InputWorkspace="__IWS",OutputWorkspace = "__IWS",LowerCutoff = 10)
-    AddSampleLogsMultiple("__IWS","CalculatedEi,CalculatedT0",str(Efixed)+','str(T0))
+    AddSampleLogsMultiple("__IWS","CalculatedEi,CalculatedT0",str(Efixed)+','+str(T0))
     return [Eguess,Efixed,T0]
   
     
