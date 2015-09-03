a09cb7259e0dac609afed555dc404a371e63e83e
mantid-publisher <mantid-developers@mantidproject.org>
2015-09-03 15:43:04 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/arcs/reduce_ARCS.py
+++ ReductionScripts/sns/arcs/reduce_ARCS.py
@@ -89,7 +89,7 @@ if __name__ == "__main__":
     #processing parameters
      # Updated vanadium run 2014-12-15 - DLA
     RawVanadium="/SNS/ARCS/CAL/2015-B/data/ARCS_65053_event.nxs"
-    ProcessedVanadium="/SNS/ARCS/shared/autoreduce/vanadium_files/van65053_no18.nxs"
+    ProcessedVanadium="/SNS/ARCS/shared/autoreduce/vanadium_files/van65053.nxs"
     HardMaskFile=''
     IntegrationRange=[0.35,0.75] #integration range for Vanadium in angstroms
     MaskBTPParameters=[]
@@ -102,7 +102,6 @@ if __name__ == "__main__":
     MaskBTPParameters.append({'Pixel': '1-12,117-128', 'Bank': '70'})
     MaskBTPParameters.append({'Pixel': '1-14,115-128', 'Bank': '71'})
     MaskBTPParameters.append({'Tube': '3', 'Bank': '3'})
-    MaskBTPParameters.append({'Bank': '18'})
 
 
     #groupingFile='/SNS/ARCS/shared/autoreduce/ARCS_2X1_grouping.xml'  #this is the grouping file, powder.xml, 2X1.xml and so on. needs the full path for this file.
