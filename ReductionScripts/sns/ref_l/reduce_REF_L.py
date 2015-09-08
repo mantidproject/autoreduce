016838ffc2651a24ab6c758a9846948ba83156be
mantid-publisher <mantid-developers@mantidproject.org>
2015-09-08 11:33:05 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/ref_l/reduce_REF_L.py
+++ ReductionScripts/sns/ref_l/reduce_REF_L.py
@@ -284,8 +284,8 @@ if len(plot_data)>1:
     plt.title(y_label)
     plt.xlabel('Q')
     plt.ylabel('Reflectivity')
-    if max(plot_data[0][2])>0:
-        plt.yscale('log')
+    #if max(plot_data[0][2])>0:
+    #    plt.yscale('log')
     if max(plot_data[0][1])>0:
         plt.xscale('log')
     plt.xlim(xmin=qmin, xmax=qmax)
