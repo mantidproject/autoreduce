ae970ac5028f38906f4392c1a6667c5286dc2dfb
mantid-publisher <mantid-developers@mantidproject.org>
2015-10-08 22:17:04 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/sequoia/reduce_SEQ.py
+++ ReductionScripts/sns/sequoia/reduce_SEQ.py
@@ -53,10 +53,10 @@ def preprocessData(filename):
     CorrectLogTimes('__IWS')
     
     # adjust time for pack B15 wired strangely
-    #ChangeBinOffset(InputWorkspace="__IWS",OutputWorkspace="__IWS",Offset=500,IndexMin=14336,IndexMax=15359)
+    ChangeBinOffset(InputWorkspace="__IWS",OutputWorkspace="__IWS",Offset=500,IndexMin=14336,IndexMax=15359)
 
     #delete all bad pulses below   10% of the average of the file.
-    FilterBadPulses(InputWorkspace="__IWS",OutputWorkspace = "__IWS",LowerCutoff = 10)
+    #FilterBadPulses(InputWorkspace="__IWS",OutputWorkspace = "__IWS",LowerCutoff = 10)
     AddSampleLogMultiple("__IWS","CalculatedEi,CalculatedT0",str(Efixed)+','+str(T0))
     return [Eguess,Efixed,T0]
   
