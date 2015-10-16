5d4bb01763d9b4f75e8405473b1f9de1983e7262
mantid-publisher <mantid-developers@mantidproject.org>
2015-10-16 09:35:05 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/powgen/reduce_PG3.py
+++ ReductionScripts/sns/powgen/reduce_PG3.py
@@ -6,7 +6,7 @@ from mantid.simpleapi import *
 import mantid
 from matplotlib import *
 cal_dir = "/SNS/PG3/IPTS-2767/shared/2015_2_11A_CAL/"
-cal_file  = os.path.join(cal_dir, "PG3_PAC_d24830_2015_08_14.cal")
+cal_file  = os.path.join(cal_dir, "PG3_AGES_d25522_2015_10_16.cal")
 char_file = os.path.join(cal_dir, "PG3_char_2015_10_15-HR-AGES.txt")
 #MODE = 0664
 
