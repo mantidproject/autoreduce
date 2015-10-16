9c62b65a3e0e595154e5b3da8d14221de7205c15
mantid-publisher <mantid-developers@mantidproject.org>
2015-10-16 09:58:04 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/nomad/reduce_NOM.py
+++ ReductionScripts/sns/nomad/reduce_NOM.py
@@ -1,18 +1,10 @@
+import hashlib
 import os
 import sys
-import shutil 
+import shutil
 sys.path.append("/opt/mantidnightly/bin")
 from mantid.simpleapi import *
 import mantid
-
-cal_dir = "/SNS/NOM/IPTS-13670/shared"
-cal_file  = os.path.join(cal_dir, "NOM_calibrate_d49884_2015_09_04.h5")
-char_file = "/SNS/NOM/shared/NOM_characterizations.txt" #os.path.join(cal_dir, "NOM_characterizations.txt")
-sam_back =     49894
-van      =     49888
-van_back =     49890
-
-#from mantidsimple import *
 
 eventFileAbs=sys.argv[1]
 outputDir=sys.argv[2]
@@ -28,12 +20,68 @@ dataSearchPath = configService.getDataSearchDirs()
 dataSearchPath.append(nexusDir)
 configService.setDataSearchDirs(";".join(dataSearchPath))
 
+# these should be options that are filled in by the calling script
+resamplex=-6000
+vanradius=0.58
+wavelengthMin=0.
+wavelengthMax=0.
+
+# determine information for vanadium caching
+wksp=LoadEventNexus(Filename=eventFileAbs, MetaDataOnly=True)
+PDLoadCharacterizations(Filename="/SNS/NOM/shared/NOM_characterizations.txt",
+                        ExpIniFilename="/SNS/NOM/IPTS-14883/shared/autoNOM2/exp.ini",
+                        OutputWorkspace="characterizations")
+PDDetermineCharacterizations(InputWorkspace=wksp,
+                             Characterizations="characterizations")
+DeleteWorkspace(str(wksp))
+DeleteWorkspace("characterizations")
+charPM = mantid.PropertyManagerDataService.retrieve('__pd_reduction_properties')
+van_run=charPM['vanadium'].value[0]
+
+vanProcessingProperties=['vanadium',
+                         'empty',
+                         'd_min',
+                         'd_max',
+                         'tof_min',
+                         'tof_max']
+vanProcessingProperties=[key+"="+charPM[key].valueAsStr for key in vanProcessingProperties]
+vanProcessingProperties.append("ResampleX="+str(resamplex))
+vanProcessingProperties.append("VanadiumRadius="+str(vanradius))
+vanProcessingProperties.append("CropWavelengthMin="+str(wavelengthMin))
+vanProcessingProperties.append("CropWavelengthMax="+str(wavelengthMax))
+vanProcessingProperties=','.join(vanProcessingProperties)
+print vanProcessingProperties
+vanProcessingProperties=hashlib.sha1(vanProcessingProperties).hexdigest()
+vanCacheName="NOM_%d_%s.nxs" % (van_run, vanProcessingProperties)
+vanCacheName=os.path.join(outputDir,vanCacheName)
+vanWkspName="NOM_"+str(van_run)
+
+if os.path.exists(vanCacheName):
+    print "Loading vanadium cache file '%s'" % vanCacheName
+    Load(Filename=vanCacheName, OutputWorkspace=vanWkspName)
+
+# process the run
 SNSPowderReduction(Instrument="NOM", RunNumber=runNumber, Extension="_event.nxs",
                    MaxChunkSize=maxChunkSize, PreserveEvents=True,PushDataPositive='AddMinimum',
-                   CalibrationFile=cal_file, CharacterizationRunsFile=char_file,
-                   BackgroundNumber=sam_back, VanadiumNumber=van,
-                   VanadiumBackgroundNumber=van_back, RemovePromptPulseWidth=50,
-                   ResampleX=-3000, BinInDspace=True, FilterBadPulses=25.,
-                   SaveAs="gsas and fullprof and pdfgetn", OutputDirectory=outputDir,
+                   CalibrationFile="/SNS/NOM/IPTS-14883/shared/NOM_calibrate_d59380_2015_10_16.h5",
+                   CharacterizationRunsFile="/SNS/NOM/shared/NOM_characterizations.txt",
+                   ExpIniFilename="/SNS/NOM/IPTS-14883/shared/autoNOM2/exp.ini",
+                   RemovePromptPulseWidth=50,
+                   ResampleX=resamplex, BinInDspace=True, FilterBadPulses=25.,
+                   CropWavelengthMin=wavelengthMin,
+                   CropWavelengthMax=wavelengthMax,
+                   SaveAs="gsas fullprof topas pdfgetn",
+                   OutputDirectory=outputDir,
                    StripVanadiumPeaks=True,
+                   VanadiumRadius=vanradius,
                    NormalizeByCurrent=True, FinalDataUnits="MomentumTransfer")
+
+# save out the vanadium cache file
+if not os.path.exists(vanCacheName):
+    SaveNexusProcessed(InputWorkspace=vanWkspName, Filename=vanCacheName)
+
+# save a picture of the normalized ritveld data
+wksp_name="NOM_"+runNumber
+SavePlot1D(InputWorkspace=wksp_name,
+           OutputFilename=os.path.join(outputDir,wksp_name+'.png'),
+           YLabel='Intensity')
