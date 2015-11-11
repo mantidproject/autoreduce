af0bdcae64df1a1c451cc20b42d7722f56ab7453
mantid-publisher <mantid-developers@mantidproject.org>
2015-11-11 11:15:05 -0500

    Updated reduction script by cron job

--- ReductionScripts/sns/corelli/reduce_CORELLI.py
+++ ReductionScripts/sns/corelli/reduce_CORELLI.py
@@ -220,11 +220,11 @@ if __name__ == "__main__":
     minn,maxx = ConvertToMDMinMaxGlobal(InputWorkspace=raw,QDimensions='Q3D',dEAnalysisMode='Elastic')
     mdraw = ConvertToMD(raw,QDimensions="Q3D",dEAnalysisMode="Elastic",Q3DFrames=Q3DFrames,
                         LorentzCorrection=LorentzCorrection,#MinValues=minn,MaxValues=maxx,
-                        MinValues='-6.1,-0.1,-2.5',MaxValues='-0.9,4.5,2.5',
+                        MinValues='-6.1,-7.1,-1.5',MaxValues='-0.9,7.1,1.5',
                         Uproj='1,0,0',Vproj='0,1,0',Wproj='0,0,1')
     mdcc  = ConvertToMD(cc,QDimensions="Q3D",dEAnalysisMode="Elastic",Q3DFrames=Q3DFrames,
                         LorentzCorrection=LorentzCorrection,#MinValues=minn,MaxValues=maxx,
-                        MinValues='-1.1,-1.1,-2.5',MaxValues='4.5,4.5,2.5',
+                        MinValues='-6.1,-7.1,-1.5',MaxValues='-0.9,7.1,1.5',
                         Uproj='1,0,0',Vproj='0,1,0',Wproj='0,0,1')
 
     # Save normalized MDs, if possible
