109bdd013d6e0a3fd8186df242f8e9a18c0ef630
mantid-publisher <mantid-developers@mantidproject.org>
2015-11-23 22:35:05 -0500

    Updated reduction script by cron job

--- ReductionScripts/sns/nomad/reduce_NOM.py
+++ ReductionScripts/sns/nomad/reduce_NOM.py
@@ -36,7 +36,7 @@ else:
 # determine information for caching
 wksp=LoadEventNexus(Filename=eventFileAbs, MetaDataOnly=True)
 PDLoadCharacterizations(Filename="/SNS/NOM/IPTS-4480/shared/characterization_files/NOM_characterizations_2015_10_15.txt",
-                        ExpIniFilename="/SNS/lustre/NOM/IPTS-14157/shared/autoNOM_again/exp.ini",
+                        ExpIniFilename="/SNS/lustre/NOM/IPTS-14157/shared/autoNOM_upstream/exp.ini",
                         OutputWorkspace="characterizations")
 PDDetermineCharacterizations(InputWorkspace=wksp,
                              Characterizations="characterizations")
@@ -98,7 +98,7 @@ SNSPowderReduction(Instrument="NOM", RunNumber=runNumber, Extension="_event.nxs"
                    MaxChunkSize=maxChunkSize, PreserveEvents=True,PushDataPositive='AddMinimum',
                    CalibrationFile="/SNS/NOM/IPTS-14157/shared/NOM_calibrate_d61868_2015_11_23.h5",
                    CharacterizationRunsFile="/SNS/NOM/IPTS-4480/shared/characterization_files/NOM_characterizations_2015_10_15.txt",
-                   ExpIniFilename="/SNS/lustre/NOM/IPTS-14157/shared/autoNOM_again/exp.ini",
+                   ExpIniFilename="/SNS/lustre/NOM/IPTS-14157/shared/autoNOM_upstream/exp.ini",
                    RemovePromptPulseWidth=50,
                    ResampleX=resamplex, BinInDspace=True, FilterBadPulses=25.,
                    CropWavelengthMin=wavelengthMin,
