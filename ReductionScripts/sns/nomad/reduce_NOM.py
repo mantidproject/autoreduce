6f71ccc0ab45ff3807451a1a4f1136591d10a561
mantid-publisher <mantid-developers@mantidproject.org>
2016-01-28 14:06:03 -0500

    Updated reduction script by cron job

--- ReductionScripts/sns/nomad/reduce_NOM.py
+++ ReductionScripts/sns/nomad/reduce_NOM.py
@@ -106,7 +106,7 @@ if van_run > 0:
 # process the run
 SNSPowderReduction(Instrument="NOM", RunNumber=runNumber, Extension="_event.nxs",
                    MaxChunkSize=maxChunkSize, PreserveEvents=True,PushDataPositive='AddMinimum',
-                   CalibrationFile="/SNS/NOM/IPTS-14060/shared/NOM_calibrate_d45623_2016_01_27.h5",
+                   CalibrationFile="/SNS/NOM/IPTS-14060/shared/NOM_calibrate_d45623_2016_01_28.h5",
                    CharacterizationRunsFile="/SNS/NOM/IPTS-4480/shared/characterization_files/NOM_characterizations_2015_10_15.txt",
                    ExpIniFilename=expiniFilename,
                    RemovePromptPulseWidth=50,
