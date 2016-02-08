78be5f2eb9b1b064f48f7e93f33a76b13d57f9f8
mantid-publisher <mantid-developers@mantidproject.org>
2016-02-08 11:05:05 -0500

    Updated reduction script by cron job

--- ReductionScripts/sns/powgen/reduce_PG3.py
+++ ReductionScripts/sns/powgen/reduce_PG3.py
@@ -24,7 +24,7 @@ dataSearchPath.append(nexusDir)
 configService.setDataSearchDirs(";".join(dataSearchPath))
 
 SNSPowderReduction(Instrument="PG3", RunNumber=runNumber, Extension="_event.nxs",
-                   PreserveEvents=True,PushDataPositive="AddMinimum",
+                   PreserveEvents=True,PushDataPositive="None",
                    CalibrationFile=cal_file, CharacterizationRunsFile=char_file,
                    LowResRef=0, RemovePromptPulseWidth=50,
                    Binning=-0.0008, BinInDspace=True, 
