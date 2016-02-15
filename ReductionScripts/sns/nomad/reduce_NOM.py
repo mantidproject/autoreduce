ee60703f05e7b9899abf3176b5c4dc48e9cd78ed
mantid-publisher <mantid-developers@mantidproject.org>
2016-02-15 15:47:04 -0500

    Updated reduction script by cron job

--- ReductionScripts/sns/nomad/reduce_NOM.py
+++ ReductionScripts/sns/nomad/reduce_NOM.py
@@ -40,7 +40,7 @@ else:
 proposalDir = '/' + '/'.join(nexusDir.split('/')[1:4])
 expiniFilename=os.path.join(proposalDir, 'shared', 'autoNOM', 'exp.ini')
 if not os.path.exists(expiniFilename):
-    expiniFilename="/SNS/lustre/NOM/IPTS-15992/shared/autoNOM/exp.ini"
+    expiniFilename="/SNS/lustre/NOM/IPTS-16107/shared/autoNOM/exp.ini"
 print "Using", expiniFilename
 
 # determine information for caching
@@ -106,7 +106,7 @@ if van_run > 0:
 # process the run
 SNSPowderReduction(Instrument="NOM", RunNumber=runNumber, Extension="_event.nxs",
                    MaxChunkSize=maxChunkSize, PreserveEvents=True,PushDataPositive='AddMinimum',
-                   CalibrationFile="/SNS/NOM/IPTS-15992/shared/NOM_calibrate_d63845_2016_02_14.h5",
+                   CalibrationFile="/SNS/NOM/IPTS-16107/shared/NOM_calibrate_d64281_2016_02_15.h5",
                    CharacterizationRunsFile="/SNS/NOM/IPTS-4480/shared/characterization_files/NOM_characterizations_2015_10_15.txt",
                    ExpIniFilename=expiniFilename,
                    RemovePromptPulseWidth=50,
