ddbe7cab5c2ac4ac268eddbd15baaf2b06537dac
mantid-publisher <mantid-developers@mantidproject.org>
2016-02-16 09:21:04 -0500

    Updated reduction script by cron job

--- ReductionScripts/sns/arcs/reduce_ARCS.py
+++ ReductionScripts/sns/arcs/reduce_ARCS.py
@@ -88,7 +88,7 @@ if __name__ == "__main__":
     numpy.seterr("ignore")#ignore division by 0 warning in plots
     #processing parameters
      # Updated vanadium run 2014-12-15 - DLA
-    RawVanadium="/SNS/ARCS/CAL/2016-A/data/ARCS_74333_event.nxs"
+    RawVanadium="/SNS/ARCS/IPTS-16075/data/ARCS_74333_event.nxs"
     ProcessedVanadium="/SNS/ARCS/shared/autoreduce/vanadium_files/van74333.nxs"
     HardMaskFile=''
     IntegrationRange=[0.35,0.75] #integration range for Vanadium in angstroms
