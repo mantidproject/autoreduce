c8f50a87f5451008d0d1db0dc6a60077004403f5
mantid-publisher <mantid-developers@mantidproject.org>
2016-02-24 17:00:12 -0500

    Updated reduction script by cron job

--- ReductionScripts/sns/cncs/reduce_CNCS.py
+++ ReductionScripts/sns/cncs/reduce_CNCS.py
@@ -101,7 +101,8 @@ HardMaskFile=''
 #IntegrationRange=[49500.0,50500.0]#integration range for Vanadium in TOF at 3.32 meV
 IntegrationRange=[90600.0,91600.0]#integration range for Vanadium in TOF at 1.0 meV
 
-MaskBTPParameters=[{'Pixel':"1-8,121-128"}]
+#MaskBTPParameters=[{'Pixel':"1-8,121-128"}]
+MaskBTPParameters=[{'Pixel':"1-40,91-128"}]
 #MaskBTPParameters.append({'Tube': '7,8', 'Bank': '50'})
 #MaskBTPParameters.append({'Bank': '36-50'})
 
