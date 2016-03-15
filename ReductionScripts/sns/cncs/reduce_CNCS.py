1902ec558dfccb410bd06e9de7124fdf79985f87
mantid-publisher <mantid-developers@mantidproject.org>
2016-03-15 15:22:05 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/cncs/reduce_CNCS.py
+++ ReductionScripts/sns/cncs/reduce_CNCS.py
@@ -1,70 +1,72 @@
 #!/usr/bin/env python
 
-import os, sys, traceback
-sys.path.insert(0,"/mnt/software/lib/python2.6/site-packages/matplotlib-1.2.0-py2.6-linux-x86_64.egg/")
-from string import *
-from numpy import *
+#imports section
+import sys, os, glob, filecmp, datetime, shutil
+sys.path.append("/SNS/CNCS/shared/autoreduce")
 from ARLibrary import * #note that ARLibrary would set mantidpath as well
-
-import sys,os
 sys.path.append("/opt/Mantid/bin")
 from mantid.simpleapi import *
 
-from matplotlib import *
-use("agg")
-from matplotlib.pyplot import *
- 
-#os.remove('/SNS/CNCS/IPTS-11820/shared/autoreduce/van101708.nxs')
 
-##-------------------------------------------------------------------------
-import numpy as np
-def GetT0FromDet(ws):
-    minAngle=30.
-    maxAngle=60.
-    alpha=437.37 #v=alpha*sqrt(Ei)
-    __tempws=CloneWorkspace(ws)
-    MaskBTP(__tempws,Bank="35-50")
-    MaskBTP(__tempws,Pixel="1-8,121-128")
-    MaskAngle(__tempws,0,minAngle)
-    MaskAngle(__tempws,maxAngle,180)
-    inst=__tempws.getInstrument()
-    sample=inst.getSample()
-    dss=sample.getDistance(inst.getSource())
-    Ei=__tempws.run()['EnergyRequest'].timeAverageValue()
-    v=alpha*np.sqrt(Ei)
-    for i in range(__tempws.getNumberHistograms()):
-        detID=__tempws.getSpectrum(i).getDetectorIDs()[0]
-        d=inst.getDetector(detID).getDistance(sample)+dss
-        offset=-1e6*d/v
-        el=__tempws.getEventList(i)
-        el.convertTof(1.,offset)
-    __binned=Rebin(__tempws,"-100,10,300",PreserveEvents=0)
-    __summed=SumSpectra(__binned)
-    y=__summed.readY(0)
-    x=__summed.readX(0)
-    bk=y[0]
-    height=y.max()
-    center=x[y.argmax()]
-    sigma=50.
-    Function='name=FlatBackground,A0={0};name=Gaussian,Height={1},PeakCentre={2},Sigma={3}'.format(bk,height,center,sigma)
-    res=Fit(Function=Function,InputWorkspace=__summed, Output='__fit')
-    DeleteWorkspace(__summed)
-    DeleteWorkspace(__tempws)
-    DeleteWorkspace(__binned)
-    T0=res[3].cell(2,1)
-    DeleteWorkspace( '__fit_NormalisedCovarianceMatrix')
-    DeleteWorkspace('__fit_Parameters')
-    DeleteWorkspace('__fit_Workspace')
-    return T0
+#parameters section
+#this part changes with web input
+MaskBTPParameters=[]
+MaskBTPParameters.append({'Pixel': '1-7,122-128'})
+MaskBTPParameters.append({'Bank': '36-50'})#8T magnet
+raw_vanadium="/SNS/CNCS/IPTS-16111/1/161099/NeXus/CNCS_161099_event.nxs"
+processed_vanadium="van161099.nxs"
+grouping="2x1" #allowed values 1x1, 2x1, 4x1, 8x1, powder
+Emin="-0.5"
+Emax="0.95"
+Estep="0.005"
+E_pars_in_mev=False
+TIB_min=""
+TIB_max=""
+T0=""
+Motor_names='huber,SERotator2,OxDilRot,CCR13VRot,SEOCRot,CCR10G2Rot,Ox2WeldRot,ThreeSampleRot'
+Temperature_names='SampleTemp,sampletemp,SensorC,SensorB,SensorA,temp5,temp8'
+create_elastic_nxspe=True #+-0.1Ei, 5 steps
+create_MDnxs=False
+a=""
+b=""
+c=""
+alpha=""
+beta=""
+gamma=""
+uVector=""
+vVector=""
 
-def GetT0FromDet_GeorgNov2015(ws):
-    Ei=ws.run()['EnergyRequest'].timeAverageValue()
-    mode=ws.run()['DoubleDiskMode'].timeAverageValue()
-    lnEi=np.log(Ei)
-    T0=157.539+lnEi*(-33.04593+lnEi*(-8.07523+lnEi*(2.2143-0.109521767*lnEi)))
-    if (mode!=1):
-        T0-=5.91
-    return T0
+#parameters not on the webpage
+#below remains unchanged
+VanadiumIntegrationRange=[84000.0,94000.0]#integration range for Vanadium in TOF at 1.0 meV
+NormalizedVanadiumEqualToOne = True
+
+
+
+
+#Reduction section
+def check_newer_script(instrument,folder):
+    """
+    Checks if reduce_instrument.py is in a certain folder.
+    It searches for all reduce_instrument*.py, takes the newest one and compares the content with
+    /SNS/instrument/shared/autoreduce/reduce_instrument.py. If there is no such file in the folder,
+    or the content has changed, it will copy reduce_instrument.py to reduce_instrument_date_and_time.py
+    in folder.
+    """
+    master_filename="/SNS/"+instrument+"/shared/autoreduce/reduce_"+instrument+".py"
+    master_filename='/SNS/users/3y9/Desktop/reduce_CNCS.py'
+    search_pattern=os.path.join(folder,"reduce_"+instrument+"*.py")
+    result=glob.glob(search_pattern)
+    newer_file_exists=True
+    if result:
+        # there are reduce_... files, get the newest
+        newest_filename=max(result,key=os.path.getctime)
+        #check content. If the same, then there is no newer file
+        newer_file_exists=not filecmp.cmp(master_filename,newest_filename)
+    if newer_file_exists:
+        new_filename=os.path.join(folder,"reduce_"+instrument+"_"+datetime.datetime.now().strftime('%Y.%m.%d_%H.%M.%S')+".py")
+        shutil.copy2(master_filename,new_filename)
+
 
 def preprocessVanadium(Raw,Processed,Parameters):
     if os.path.isfile(Processed):
@@ -72,168 +74,157 @@ def preprocessVanadium(Raw,Processed,Parameters):
         dictvan={'UseProcessedDetVan':'1','DetectorVanadiumInputWorkspace':'__VAN'}
     else:
         LoadEventNexus(Filename=Raw,OutputWorkspace="__VAN",Precount=0)
-        #ChangeBinOffset(InputWorkspace="__VAN",OutputWorkspace="__VAN",Offset=500,IndexMin=54272,IndexMax=55295) # adjust time for pack C17 wired backward
         for d in Parameters:
             MaskBTP(Workspace="__VAN",**d)
-        dictvan={'SaveProcessedDetVan':'1','DetectorVanadiumInputWorkspace':'__VAN','SaveProcDetVanFilename':Processed}
+        dictvan={'SaveProcessedDetVan':'1',
+                 'DetectorVanadiumInputWorkspace':'__VAN',
+                 'SaveProcDetVanFilename':Processed,
+                 'UseBoundsForDetVan':'1',
+                 'DetVanIntRangeHigh':VanadiumIntegrationRange[1],
+                 'DetVanIntRangeLow':VanadiumIntegrationRange[0],
+                 'DetVanIntRangeUnits':'TOF'}
     return dictvan
 
-##-------------------------------------------------------------------------
+def preprocessData(filename):
+    dictdata={}    
+    __IWS=LoadEventNexus(filename)
+    Ei=__IWS.getRun()['EnergyRequest'].firstValue()
+    t0=preprocesst0(Ei,__IWS)
+    tibmin,tibmax=preprocessTIB(Ei,__IWS)
+    ETransfer=preprocessEnergyTransfer(Ei)
+    dictdata['SampleInputWorkspace']='__IWS'
+    dictdata['SampleInputMonitorWorkspace']='__IWS'
+    dictdata['UseIncidentEnergyGuess']='1'
+    dictdata['IncidentEnergyGuess']=Ei
+    dictdata['TimeZeroGuess']=t0
+    dictdata['EnergyTransferRange']=ETransfer
+    dictdata['TimeIndepBackgroundSub']=True
+    dictdata['TibTofRangeStart']=tibmin
+    dictdata['TibTofRangeEnd']=tibmax
+    dictdata['IncidentBeamNormalisation']='ByCurrent'
+    return dictdata
 
-config['default.facility']="SNS"
-nexus_file=sys.argv[1]
-output_directory=sys.argv[2]
+def preprocesst0(Eguess,ws):
+    try:
+        t0=float(T0)
+    except ValueError:
+        mode=ws.run()['DoubleDiskMode'].timeAverageValue()
+        if (mode==1):
+            _Ei,_FMP,_FMI,t0=GetEi(ws)
+        else:
+            t0-=5.91
+    AddSampleLog(Workspace=ws,LogName="CalculatedT0",LogText=str(t0),LogType="Number")
+    return t0
 
-seterr("ignore") #ignore division by 0 warning in plots
+def preprocessTIB(EGuess,ws):
+    try:
+        tibmin=float(TIB_min)
+        tibmax=float(TIB_max)
+    except ValueError:
+        if EGuess<50:
+            tibmin,tibmax=SuggestTibCNCS(EGuess)
+        else:
+            tibmin=5000
+            tibmax=15000
+        if (abs(EGuess-12)<0.1):
+            tibmin,tibmax=[20500.0,21500.0]
+        if (abs(EGuess-25)<0.1):
+            tibmin,tibmax=[11000.0,15000.0]  
+    AddSampleLogMultiple(ws,"TIBmin,TIBmax",str(tibmin)+','+str(tibmax))
+    return (tibmin,tibmax)
 
-#RawVanadium="/SNS/CNCS/IPTS-14518/1/159854/NeXus/CNCS_159854_event.nxs"
-RawVanadium="/SNS/CNCS/IPTS-16111/1/161099/NeXus/CNCS_161099_event.nxs"#8T magnet
-#RawVanadium="/SNS/CNCS/IPTS-4654/29/151026/NeXus/CNCS_151026_event.nxs"
-#RawVanadium="/SNS/CNCS/IPTS-14518/0/140450/NeXus/CNCS_140450_event.nxs"
-#RawVanadium="/SNS/CNCS/IPTS-4654/25/137573/NeXus/CNCS_137573_event.nxs"
-#RawVanadium="/SNS/CNCS/IPTS-4654/24/123012/NeXus/CNCS_123012_event.nxs"
-#RawVanadium="/SNS/CNCS/IPTS-13623/0/115557/NeXus/CNCS_115557_event.nxs"
-#RawVanadium="/SNS/CNCS/IPTS-4654/23/109039/NeXus/CNCS_109039_event.nxs"
-#RawVanadium="/SNS/CNCS/IPTS-4654/22/101708/NeXus/CNCS_101708_event.nxs"
-#ProcessedVanadium="van101708both.nxs"
-#ProcessedVanadium="van123012.nxs"
-#ProcessedVanadium="van.nxs"
-ProcessedVanadium="van161099.nxs"#8T magnet
-HardMaskFile=''
-#IntegrationRange=[49500.0,50500.0]#integration range for Vanadium in TOF at 3.32 meV
-IntegrationRange=[84000.0,94000.0]#integration range for Vanadium in TOF at 1.0 meV
+def preprocessEnergyTransfer(EGuess):
+    try:
+        emin=float(Emin)
+        emax=float(Emax)
+        estep=float(Estep)
+        if E_pars_in_mev:
+            return [emin,estep,emax]
+        else:
+            return [emin*EGuess,estep*EGuess,emax*EGuess]
+    except ValueError:
+        return [-0.5*EGuess,0.01*EGuess,0.95*EGuess]
 
-#MaskBTPParameters=[{'Pixel':"1-8,121-128"}]
-MaskBTPParameters=[{'Pixel':"1-43,95-128"}]#8T magnet
-#MaskBTPParameters.append({'Tube': '7,8', 'Bank': '50'})
-MaskBTPParameters.append({'Bank': '36-50'})#8T magnet
+def preprocessGrouping(ws,outdir):
+    if grouping in ['2x1', '4x1', '8x1']:
+        dictgrouping={'GroupingFile':"/SNS/CNCS/shared/autoreduce/CNCS_"+grouping+".xml"}
+    elif grouping=='powder':
+        GenerateGroupingPowder(InputWorkspace=ws,AngleStep=0.5, GroupingFilename=outdir+'powdergroupfile.xml')
+        dictgrouping={'GroupingFile':outdir+'powdergroupfile.xml'}
+    else:
+        dictgrouping={'GroupingFile':''}
+    return dictgrouping
 
-w=Load(nexus_file)
-EGuess=w.getRun()['EnergyRequest'].firstValue()
 
-if EGuess<50:
-    tib=SuggestTibCNCS(EGuess)
-if (abs(EGuess-12)<0.1):
-    tib=[20500.0,21500.0]
-#if (abs(EGuess-15)<0.1):
-#    tib=[15200.0,16500.0]
-if (abs(EGuess-25)<0.1):
-    tib=[11000.0,15000.0]
-if (abs(EGuess-55)<0.1):
-    tib=[5000.0,15000.0]
+
+
+if __name__ == "__main__":    
+    #check number of arguments
+    if (len(sys.argv) != 3): 
+        print "autoreduction code requires a filename and an output directory"
+        sys.exit()
+    if not(os.path.isfile(sys.argv[1])):
+        print "data file ", sys.argv[1], " not found"
+        sys.exit()
+
+    config['default.facility']="SNS"
+    nexus_file=sys.argv[1]
+    output_directory=sys.argv[2]
     
-#tib=[24000,29000]
+    check_newer_script("CNCS",output_directory)
+    
+    DGSdict=preprocessVanadium(raw_vanadium,output_directory+processed_vanadium,MaskBTPParameters)
+    datadict=preprocessData(nexus_file)
+    groupdict=preprocessGrouping("__IWS",output_directory)
+    DGSdict.update(datadict)
+    DGSdict.update(groupdict)
+    DGSdict['OutputWorkspace']='reduce'
 
-t0=GetT0FromDet_GeorgNov2015(w)
-#print 'for Ei=',EGuess,'use T0=',t0
-#t0=GetT0FromDet(w)
+    DgsReduction(**DGSdict)
 
-#t0=110  #Ei=3.0 meV
-#t0=116  #Ei=2.5 meV
+    if DGSdict.has_key('SaveProcessedDetVan') and NormalizedVanadiumEqualToOne:
+        filename=DGSdict['SaveProcDetVanFilename']
+        LoadNexus(Filename=filename,OutputWorkspace="__VAN")
+        datay = mtd['__VAN'].extractY()
+        meanval = float(datay[datay>0].mean())
+        CreateSingleValuedWorkspace(OutputWorkspace='__meanval',DataValue=meanval)
+        Divide(LHSWorkspace='__VAN',RHSWorkspace='__meanval',OutputWorkspace='__VAN') #Divide the vanadium by the mean
+        Multiply(LHSWorkspace='reduce',RHSWorkspace='__meanval',OutputWorkspace='reduce') #multiple by the mean of vanadium Normalized data = Data / (Van/meanvan) = Data *meanvan/Van
+        SaveNexus(InputWorkspace="__VAN", Filename= filename) 
+        os.chmod(filename,0444)
 
-#if (abs(EGuess-12)<0.1):
-#	t0=65
-#if (abs(EGuess-25)<0.1):
-#	t0=30
-#if (abs(EGuess-55)<0.1):
-#	t0=10
+    if create_elastic_nxspe:
+        DGSdict['OutputWorkspace']='reduce_elastic'
+        EGuess=DGSdict['IncidentEnergyGuess']
+        DGSdict['EnergyTransferRange']=[-0.1*EGuess,0.04*EGuess,0.1*EGuess]
+        DgsReduction(**DGSdict)
 
-DGSdict=preprocessVanadium(RawVanadium,output_directory+ProcessedVanadium,MaskBTPParameters)
-DGSdict['SampleInputFile']=nexus_file
-DGSdict['EnergyTransferRange']=[-0.95*EGuess,0.005*EGuess,0.95*EGuess]  #Typical values are -0.5*EGuess, 0.005*EGuess, 0.95*EGuess
-DGSdict['HardMaskFile']=HardMaskFile
-DGSdict['GroupingFile']="/SNS/CNCS/shared/autoreduce/CNCS_2x1.xml"
-#DGSdict['GroupingFile']="/SNS/CNCS/shared/autoreduce/CNCS_8x1.xml"
-DGSdict['IncidentBeamNormalisation']='ByCurrent'  
-DGSdict['UseBoundsForDetVan']='1'
-DGSdict['DetVanIntRangeHigh']=IntegrationRange[1]
-DGSdict['DetVanIntRangeLow']=IntegrationRange[0]
-DGSdict['DetVanIntRangeUnits']='TOF'
-DGSdict['OutputWorkspace']='reduce'
-DGSdict['TibTofRangeStart']=tib[0]
-DGSdict['TibTofRangeEnd']=tib[1]
-DGSdict['TimeIndepBackgroundSub']=True
-#DGSdict['TimeIndepBackgroundSub']=False
-DGSdict['IncidentEnergyGuess']=EGuess
-DGSdict['TimeZeroGuess']=t0
-DGSdict['UseIncidentEnergyGuess']=True
-
-DgsReduction(**DGSdict)
-NormalizedVanadiumEqualToOne = True
-if DGSdict.has_key('SaveProcessedDetVan') and NormalizedVanadiumEqualToOne:
-    filename=DGSdict['SaveProcDetVanFilename']
-    LoadNexus(Filename=filename,OutputWorkspace="__VAN")
-    datay = mtd['__VAN'].extractY()
-    meanval = float(datay[datay>0].mean())
-    CreateSingleValuedWorkspace(OutputWorkspace='__meanval',DataValue=meanval)
-    Divide(LHSWorkspace='__VAN',RHSWorkspace='__meanval',OutputWorkspace='__VAN') #Divide the vanadium by the mean
-    Multiply(LHSWorkspace='reduce',RHSWorkspace='__meanval',OutputWorkspace='reduce') #multiple by the mean of vanadium Normalized data = Data / (Van/meanvan) = Data *meanvan/Van
-    SaveNexus(InputWorkspace="__VAN", Filename= filename) 
-
-filename=output_directory+ProcessedVanadium
-os.chmod(filename,0444)
-
-filename = os.path.split(nexus_file)[-1]
-#run_number = filename.split('_')[1]
-
-#added Feb 10, 2014 AS GE
-elog=ExperimentLog()
-elog.setLogList('EnergyRequest')
-elog.setSimpleLogList("EnergyRequest")
-#elog.setSERotOptions('CCR10G2Rot')
-#elog.setSERotOptions('SERotator2')
-elog.setSERotOptions('Ox2WeldRot')
-#elog.setSERotOptions('ThreeSampleRot')
-#elog.setSERotOptions('SERotator2,OxDilRot,CCR13VRot,FatSamVRot,SEOCRot,huber,CCR10G2Rot')
-#elog.setSETempOptions('SampleTemp,sampletemp,SensorC,SensorB,SensorA')
-elog.setSETempOptions('SensorC')
-elog.setFilename(output_directory+'experiment_log.csv')
-
-s1=elog.save_line('reduce')
-
-# Get Angle
-#s1=mtd["reduce"].getRun()['CCR10G2Rot'].value[0]
-#s1=mtd["reduce"].getRun()['huber'].value[0]
-#s1=mtd["reduce"].getRun()['FatSamVRot'].value[0]
-#s1=mtd["reduce"].getRun()['SERotator2'].value[0]
-s1=mtd["reduce"].getRun()['Ox2WeldRot'].value[0]
-#s1=mtd["reduce"].getRun()['ThreeSampleRot'].value[0]
-#s1=0
-roundedvalue = "%.1f" % s1
-valuestringwithoutdot = str(roundedvalue).replace('.', 'p')
-
-run_number = os.path.splitext(os.path.splitext(filename.split('_')[1])[0])[0]
-processed_filename = os.path.join(output_directory, "CNCS_" + run_number + "_" + valuestringwithoutdot + "_spe.nxs")
-nxspe_filename=os.path.join(output_directory, "CNCS_" + run_number + "_" + valuestringwithoutdot + ".nxspe")
-
-# Save a file
-SaveNexus(Filename=processed_filename, InputWorkspace="reduce")
-SaveNXSPE(Filename=nxspe_filename, InputWorkspace="reduce", Psi=str(s1), KiOverKfScaling='1')
-os.chmod(nxspe_filename,0664)
-# make a pretty image
-#minvals,maxvals=ConvertToMDMinMaxGlobal('reduce','|Q|','Direct')
-#xmin=minvals[0]
-#xmax=maxvals[0]
-#xstep=(xmax-xmin)*0.01
-#ymin=minvals[1]
-#ymax=maxvals[1]
-#ystep=(ymax-ymin)*0.01
-#x=arange(xmin,xmax,xstep)
-#y=arange(ymin,ymax,ystep)
-#X,Y=meshgrid(x,y)
+    elog=ExperimentLog()
+    elog.setLogList('EnergyRequest,CalculatedT0,TIBmin,TIBmax')
+    elog.setSimpleLogList("EnergyRequest,CalculatedT0,TIBmin,TIBmax")
+    elog.setSERotOptions(Motor_names)
+    elog.setSETempOptions(Temperature_names)
+    elog.setFilename(output_directory+'experiment_log.csv')
 
 
-#MD=ConvertToMD('reduce',QDimensions='|Q|',dEAnalysisMode='Direct',MinValues=minvals,MaxValues=maxvals)
-#ad0='|Q|,'+str(xmin)+','+str(xmax)+',100'
-#ad1='DeltaE,'+str(ymin)+','+str(ymax)+',100'
-#MDH=BinMD(InputWorkspace=MD,AlignedDim0=ad0,AlignedDim1=ad1)
-#d=MDH.getSignalArray()
-#ne=MDH.getNumEventsArray()
-#dne=d/ne
-
-#Zm=ma.masked_where(ne==0,dne)
-#pcolormesh(X,Y,log(Zm),shading='gouraud')
-#xlabel('|Q| ($\AA^{-1}$)')
-#ylabel('E (meV)')
-#savefig(processed_filename+'.png',bbox_inches='tight')
+    run_number =mtd["reduce"].getRun()['run_number'].value
+    if groupdict['GroupingFile']==output_directory+'powdergroupfile.xml':
+        nxspe_filename=os.path.join(output_directory, "CNCS_" + run_number + "_powder.nxspe")
+        SaveNXSPE(Filename=nxspe_filename, InputWorkspace="reduce", Psi="0", KiOverKfScaling='1',ParFile=output_directory+'powdergroupfile.par')
+        os.chmod(nxspe_filename,0664)
+        if create_elastic_nxspe:
+            nxspe_filename=os.path.join(output_directory, "elastic/CNCS_" + run_number + "_elastic_powder.nxspe")
+            SaveNXSPE(Filename=nxspe_filename, InputWorkspace="reduce_elastic", Psi="0", KiOverKfScaling='1',ParFile=output_directory+'powdergroupfile.par')
+            os.chmod(nxspe_filename,0664)
+    else:
+        s1=elog.save_line('reduce')
+        roundedvalue = "%.1f" % s1
+        valuestringwithoutdot = str(roundedvalue).replace('.', 'p')
+        nxspe_filename=os.path.join(output_directory, "CNCS_" + run_number + "_" + valuestringwithoutdot + ".nxspe")
+        SaveNXSPE(Filename=nxspe_filename, InputWorkspace="reduce", Psi=str(s1), KiOverKfScaling='1')     
+        os.chmod(nxspe_filename,0664)
+        if create_elastic_nxspe:
+            nxspe_filename=os.path.join(output_directory, "elastic/CNCS_" + run_number + "_" + valuestringwithoutdot + "_elastic.nxspe")
+            SaveNXSPE(Filename=nxspe_filename, InputWorkspace="reduce_elastic", Psi="0", KiOverKfScaling='1')
+            os.chmod(nxspe_filename,0664)
 
