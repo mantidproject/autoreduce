dae88ddb54c95eecaa518f02fa3121db37467b94
mantid-publisher <mantid-developers@mantidproject.org>
2016-03-23 15:16:06 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/cncs/reduce_CNCS.py
+++ ReductionScripts/sns/cncs/reduce_CNCS.py
@@ -11,6 +11,7 @@ from mantid.simpleapi import *
 #this part changes with web input
 MaskBTPParameters=[]
 MaskBTPParameters.append({'Pixel': '1-43,95-128'})
+MaskBTPParameters.append({'Bank': '36-50'})
 
 MaskBTPParameters.append({'Pixel': '1-43,95-128'})
 #MaskBTPParameters.append({'Pixel': '1-7,122-128'})
