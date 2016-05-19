a62bca6126d2340a50fbda1207882e0df3448786
mantid-publisher <mantid-developers@mantidproject.org>
2016-05-19 08:12:08 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/powgen/sumRun_PG3.py
+++ ReductionScripts/sns/powgen/sumRun_PG3.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 import nxs, os, numpy, sys, posixpath, logging, csv
+import sys
+sys.path.append("/opt/Mantid/bin")
+
 #import xml.utils.iso8601, ConfigParser
 import ConfigParser
 from datetime import datetime
