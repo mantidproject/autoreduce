2e5fe78d9c21d10c208bed5c4806682a1f5110ae
mantid-publisher <mantid-developers@mantidproject.org>
2016-05-20 16:33:08 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/usans/reduce_USANS.py
+++ ReductionScripts/sns/usans/reduce_USANS.py
@@ -85,8 +85,8 @@ if __name__ == "__main__":
     for item in mtd['USANS'].getRun().getProperties():
         if item.name.startswith("BL1A:Mot:") and not item.name.endswith(".RBV"):
             stats = item.getStatistics()
-            print abs(stats.mean)
-            if abs(stats.standard_deviation/item.getStatistics().mean)>0.01:
+            if abs(stats.mean)>0 and abs(stats.standard_deviation/item.getStatistics().mean)>0.01 \
+            or abs(stats.mean)==0 and abs(stats.standard_deviation)>0.0:
                 scan_var = item.name
                 short_name = item.name.replace("BL1A:Mot:","")
 
