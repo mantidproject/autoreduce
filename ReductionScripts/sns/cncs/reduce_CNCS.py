46b73c26268a373d0269f533d8892f1906fda3d2
mantid-publisher <mantid-developers@mantidproject.org>
2016-05-20 09:39:06 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/cncs/reduce_CNCS.py
+++ ReductionScripts/sns/cncs/reduce_CNCS.py
@@ -20,9 +20,9 @@ MaskBTPParameters.append({'Bank': '35-50'})
 raw_vanadium="/SNS/CNCS/IPTS-16057/0/173037/NeXus/CNCS_173037_event.nxs"
 processed_vanadium="van173037_oneside_powder.nxs"
 VanadiumIntegrationRange=[52000.0,53000.0]#integration range for Vanadium in TOF at 1.0 meV
-grouping="powder" #allowed values 1x1, 2x1, 4x1, 8x1, 8x2 powder
+grouping="2x1" #allowed values 1x1, 2x1, 4x1, 8x1, 8x2 powder
 Emin="-0.2"
-Emax="0.9"
+Emax="0.95"
 Estep="0.005"
 E_pars_in_mev=False
 TIB_min=""
