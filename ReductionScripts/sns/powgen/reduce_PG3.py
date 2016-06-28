a3dd2413a6d19b62e76ec454b78112ce88a19b6a
mantid-publisher <mantid-developers@mantidproject.org>
2016-06-28 13:39:06 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/powgen/reduce_PG3.py
+++ ReductionScripts/sns/powgen/reduce_PG3.py
@@ -1,8 +1,7 @@
 import os
 import sys
-import shutil 
 sys.path.insert(0,"/mnt/software/lib/python2.6/site-packages/matplotlib-1.2.0-py2.6-linux-x86_64.egg/")
-sys.path.append("/opt/Mantid/bin")
+sys.path.append("/opt/mantidnightly/bin")
 from mantid.simpleapi import *
 import mantid
 from matplotlib import *
@@ -11,45 +10,60 @@ cal_file  = os.path.join(cal_dir, "PG3_OC_d28334_2016_05_25.h5")
 char_file = os.path.join(cal_dir, "PG3_char_2016_05_24-HR-OC-10mm.txt")
 #MODE = 0664
 
-#from mantidsimple import *
-
 eventFileAbs=sys.argv[1]
 outputDir=sys.argv[2]+'/'
 
+eventFileAbs='/SNS/PG3/IPTS-15653/0/28395/NeXus/PG3_28395_event.nxs'
+eventFileAbs='/SNS/PG3/IPTS-17223/nexus/PG3_29053.nxs.h5'
+outputDir='/tmp'
+
 eventFile = os.path.split(eventFileAbs)[-1]
 nexusDir = eventFileAbs.replace(eventFile, '')
-runNumber = eventFile.split('_')[1]
+runNumber = eventFile.split('_')[-1].split('.')[0]
 configService = mantid.config
 dataSearchPath = configService.getDataSearchDirs()
 dataSearchPath.append(nexusDir)
 configService.setDataSearchDirs(";".join(dataSearchPath))
 
-SNSPowderReduction(Instrument="PG3", RunNumber=runNumber, Extension="_event.nxs",
+SNSPowderReduction(Filename=eventFileAbs,
                    PreserveEvents=True,PushDataPositive="AddMinimum",
                    CalibrationFile=cal_file, CharacterizationRunsFile=char_file,
                    LowResRef=0, RemovePromptPulseWidth=50,
-                   Binning=-0.0008, BinInDspace=True, 
+                   Binning=-0.0008, BinInDspace=True,
                    BackgroundSmoothParams="5,2",
-                   FilterBadPulses=10,
+                   FilterBadPulses=0.,  # TODO turned off for now
                    ScaleData =100,
                    SaveAs="gsas topas and fullprof", OutputDirectory=outputDir,
                    FinalDataUnits="dSpacing")
 
-SavePlot1D(InputWorkspace="PG3_"+runNumber, OutputFilename=outputDir+"PG3_"+runNumber+'.png',  YLabel='Intensity')
+GeneratePythonScript(InputWorkspace="PG3_"+runNumber,
+                     Filename=os.path.join(outputDir,"PG3_"+runNumber+'.py'))
+ConvertUnits(InputWorkspace='PG3_'+runNumber, OutputWorkspace='PG3_'+runNumber,
+    Target='dSpacing',
+    EMode='Elastic')
 
-#dirList=os.listdir(outputDir)
-#for fname in dirList:
-#  os.chmod(os.path.join(outputdir, fname), MODE)
+# interactive plots
+import matplotlib.pyplot as plt
+import plotly.offline as pltly
+wksp = mtd['PG3_'+runNumber]
+fig, ax = plt.subplots()
+ax.plot(wksp.readX(0)[:-1], wksp.readY(0)
+        #edgecolor=ec,
+        #linewidth=ew*width_scale
+)
+ax.set_ylabel(wksp.YUnitLabel())
+xunit = wksp.getAxis(0).getUnit()
+ax.set_xlabel('%s (%s)' % (xunit.caption(), xunit.symbol().utf8()))
+ax.grid()
 
-#fileName= "PG3_" + runNumber + "_REDUCED.gsa"
-#outputFile=outputDir+"/"+fileName
-#f=open(outputFile, 'w')
-#f.write('POWGEN auto data reduction results')
-#
-#fileName2= "PG3_" + runNumber + "_REDUCED2.gsa"
-#outputFile2=outputDir+"/"+fileName2
-#f2=open(outputFile2, 'w')
-#f2.write('More POWGEN auto data reduction results')
-#
-#print outputFile + " is created"
-#print outputFile2 + " is created"
+if True:  # full html page
+    pltly.plot_mpl(fig, show_link=False,
+                   filename=os.path.join('/tmp','PG3_%s.html' % runNumber))
+else:  # post to the plot server
+    div = pltly.plot_mpl(fig, show_link=False,
+                         output_type='div', include_plotlyjs=False)
+    files = {'file':div}
+
+    from postprocessing.publish_plot import publish_plot
+    request = publish_plot('PG3', runNumber, files)
+    print "post returned %d" % request.status_code
