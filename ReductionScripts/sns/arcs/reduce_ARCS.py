2d04bc9d1bbd9604fd3171994e54c1d5d45c31c3
mantid-publisher <mantid-developers@mantidproject.org>
2016-07-15 17:09:05 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/arcs/reduce_ARCS.py
+++ ReductionScripts/sns/arcs/reduce_ARCS.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     #processing parameters
      # Updated vanadium run 2014-12-15 - DLA
     RawVanadium="/SNS/ARCS/IPTS-16075/data/ARCS_81706_event.nxs"
-    ProcessedVanadium="/SNS/ARCS/shared/autoreduce/vanadium_files/van81706_no6_elastic.nxs"
+    ProcessedVanadium="/SNS/ARCS/shared/autoreduce/vanadium_files/van81706_no6.nxs"
     HardMaskFile=''
     IntegrationRange=[0.35,0.75] #integration range for Vanadium in angstroms
     MaskBTPParameters=[]
@@ -63,7 +63,7 @@ if __name__ == "__main__":
             processed_van_file = os.path.join(outdir, ProcessedVanadium)
         DGSdict=preprocessVanadium(RawVanadium, processed_van_file, MaskBTPParameters)
 
-        EnergyTransferRange = [-0.08*EGuess,0.01*EGuess,0.08*EGuess] #Energy Binning
+        EnergyTransferRange = [-0.5*EGuess,0.01*EGuess,0.95*EGuess] #Energy Binning
         reduceMono(
             DGSdict, Ei, T0, EnergyTransferRange, 
             HardMaskFile, groupingFile, IntegrationRange,
