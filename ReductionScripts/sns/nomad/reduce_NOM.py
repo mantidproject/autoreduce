e352db60401fdbec42f253a35c842646d54d5ac0
mantid-publisher <mantid-developers@mantidproject.org>
2016-08-26 11:43:19 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/nomad/reduce_NOM.py
+++ ReductionScripts/sns/nomad/reduce_NOM.py
@@ -39,7 +39,7 @@ else:
 proposalDir = '/' + '/'.join(nexusDir.split('/')[1:4])
 expiniFilename=os.path.join(proposalDir, 'shared', 'autoNOM', 'exp.ini')
 if not os.path.exists(expiniFilename):
-    expiniFilename="/SNS/lustre/NOM/IPTS-16464/shared/exp.ini"
+    expiniFilename="/SNS/lustre/NOM/IPTS-16464/shared/autoNOM/exp.ini"
 print "Using", expiniFilename
 
 # determine information for caching
