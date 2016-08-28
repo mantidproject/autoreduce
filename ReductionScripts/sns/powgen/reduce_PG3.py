cb45115c54c12a568dae356ea6e3e32c62f80b27
mantid-publisher <mantid-developers@mantidproject.org>
2016-08-28 14:24:12 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/powgen/reduce_PG3.py
+++ ReductionScripts/sns/powgen/reduce_PG3.py
@@ -5,7 +5,7 @@ from mantid.simpleapi import *
 import mantid
 cal_dir = "/SNS/PG3/shared/CALIBRATION/2016_2_11A_CAL/"
 cal_file  = os.path.join(cal_dir, "PG3_OC_d35725_2016_08_24.cal")
-char_file = os.path.join(cal_dir, "PG3_char_2016_08_28-OC-8mm.txt")
+char_file = os.path.join(cal_dir, "PG3_char_2016_08_28-HR-OC-8mm.txt")
 #MODE = 0664
 
 eventFileAbs=sys.argv[1]
