204406a3f4319218077e26e025680e3570ee94b0
mantid-publisher <mantid-developers@mantidproject.org>
2016-09-12 13:32:07 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/sequoia/reduce_SEQ.py
+++ ReductionScripts/sns/sequoia/reduce_SEQ.py
@@ -85,7 +85,7 @@ if __name__ == "__main__":
     MaskBTPParameters.append({'Tube': '2-4', 'Pixel': '32-35', 'Bank': '88'})
     MaskBTPParameters.append({'Pixel': '1-74', 'Bank': '38-74'})
     MaskBTPParameters.append({'Pixel': '60-128', 'Bank': '114-150'})
-    MaskBTPParameters.append({'Bank': '68-74,104-113,144-150'})
+    MaskBTPParameters.append({'Bank': '68-74,109-113,144-150'})
  
     #uninstalled packs at far left
     #MaskBTPParameters.append({'Bank':"114,115,75,76,38,39"})
