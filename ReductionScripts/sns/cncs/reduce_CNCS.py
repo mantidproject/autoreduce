12ccdab611256ded666402df00f4edf8994a115b
mantid-publisher <mantid-developers@mantidproject.org>
2016-11-11 08:25:06 -0500

    Updated reduction script by cron job

--- ReductionScripts/sns/cncs/reduce_CNCS.py
+++ ReductionScripts/sns/cncs/reduce_CNCS.py
@@ -39,7 +39,7 @@ beta="90.0"
 gamma="90.0"
 uVector="0,0,1"
 vVector="1,1,0"
-sub_directory="Cooling_Triton_Nano"
+sub_directory="50mK_Triton_Nano"
 
 #parameters not on the webpage
 #below remains unchanged
