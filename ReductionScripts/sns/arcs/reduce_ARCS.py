71bd19ffe8944771deb18e6409f15a8cf00e2528
mantid-publisher <mantid-developers@mantidproject.org>
2016-11-19 17:03:05 -0500

    Updated reduction script by cron job

--- ReductionScripts/sns/arcs/reduce_ARCS.py
+++ ReductionScripts/sns/arcs/reduce_ARCS.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     #processing parameters
      # Updated vanadium run 2014-12-15 - DLA
     RawVanadium="/SNS/ARCS/IPTS-17350/data/ARCS_87355_event.nxs"
-    ProcessedVanadium="/SNS/ARCS/shared/autoreduce/vanadium_files/van87355_b.nxs"
+    ProcessedVanadium="/SNS/ARCS/shared/autoreduce/vanadium_files/van87355_c.nxs"
     HardMaskFile=''
     IntegrationRange=[0.35,0.75] #integration range for Vanadium in angstroms
     MaskBTPParameters=[]
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     MaskBTPParameters.append({'Pixel': '1-12,117-128', 'Bank': '70'})
     MaskBTPParameters.append({'Pixel': '1-14,115-128', 'Bank': '71'})
     MaskBTPParameters.append({'Bank': '16,30,55,97'})
-    MaskBTPParameters.append({'Tube': '6', 'Bank': '21'})
+    MaskBTPParameters.append({'Tube': '5,6', 'Bank': '21'})
     MaskBTPParameters.append({'Tube': '1,2,5-8', 'Bank': '23'})
     MaskBTPParameters.append({'Tube': '2', 'Bank': '49'})
     MaskBTPParameters.append({'Tube': '1', 'Bank': '52'})
@@ -27,6 +27,7 @@ if __name__ == "__main__":
     MaskBTPParameters.append({'Tube': '8', 'Bank': '105'})
     MaskBTPParameters.append({'Tube': '2,3,8', 'Bank': '7'})
     MaskBTPParameters.append({'Tube': '6', 'Bank': '10'})
+    MaskBTPParameters.append({'Tube': '2', 'Bank': '24'})
 
 
     #groupingFile='/SNS/ARCS/shared/autoreduce/ARCS_2X1_grouping.xml'  #this is the grouping file, powder.xml, 2X1.xml and so on. needs the full path for this file.
