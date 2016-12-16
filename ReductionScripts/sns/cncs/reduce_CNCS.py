444b47793c66d7033797dac1b4f2498d6d6982a2
mantid-publisher <mantid-developers@mantidproject.org>
2016-12-16 08:00:09 -0500

    Updated reduction script by cron job

--- ReductionScripts/sns/cncs/reduce_CNCS.py
+++ ReductionScripts/sns/cncs/reduce_CNCS.py
@@ -12,6 +12,7 @@ from mantid.simpleapi import *
 MaskBTPParameters=[]
 MaskBTPParameters.append({'Pixel': '1-8'})
 MaskBTPParameters.append({'Pixel': '120-128'})
+MaskBTPParameters.append({'Bank': '38-50'})
 
 #MaskBTPParameters.append({'Pixel': '1-43,95-128'})
 #MaskBTPParameters.append({'Pixel': '1-7,122-128'})
@@ -19,7 +20,7 @@ MaskBTPParameters.append({'Pixel': '120-128'})
 raw_vanadium="/SNS/CNCS/IPTS-16111/5/201975/NeXus/CNCS_201975_event.nxs"
 processed_vanadium="van201975.nxs"
 VanadiumIntegrationRange=[49500.0,50500.0]#integration range for Vanadium in TOF at 1.0 meV
-grouping="8x1" #allowed values 1x1, 2x1, 4x1, 8x1, 8x2 powder
+grouping="powder" #allowed values 1x1, 2x1, 4x1, 8x1, 8x2 powder
 Emin="-0.2"
 Emax="0.95"
 Estep="0.005"
@@ -29,7 +30,7 @@ TIB_max=""
 T0=""
 Motor_names="huber,SERotator2,OxDilRot,CCR13VRot,SEOCRot,CCR10G2Rot,Ox2WeldRot,ThreeSampleRot"
 Temperature_names="SampleTemp,sampletemp,SensorC,SensorB,SensorA,temp5,temp8,sensor0normal"
-create_elastic_nxspe=True #+-0.1Ei, 5 steps
+create_elastic_nxspe=False #+-0.1Ei, 5 steps
 create_MDnxs=False
 a="4.655"
 b="4.655"
