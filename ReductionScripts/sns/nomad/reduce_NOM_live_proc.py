fc9cef658e0b4fa7885740cd9e24059f6f2a5637
mantid-publisher <mantid-developers@mantidproject.org>
2017-01-31 08:47:07 -0500

    Updated reduction script by cron job

---
+++ ReductionScripts/sns/nomad/reduce_NOM_live_proc.py
@@ -0,0 +1,28 @@
+import mantid
+from mantid import simpleapi
+import os
+
+cal_dir = '/SNS/NOM/shared/CALIBRATION/2016_2_1B_CAL/'
+cal_file  = os.path.join(cal_dir, 'NOM_d85746_2016_11_21_shifter.h5')
+char_file = os.path.join(cal_dir, 'NOM_char_2016_12_13-rietveld.txt')
+
+mantid.logger.information('Number events = %d' % input.getNumberEvents())
+
+simpleapi.PDLoadCharacterizations(Filename=char_file,
+                                  OutputWorkspace='characterizations')
+simpleapi.PDDetermineCharacterizations(InputWorkspace=input,
+                                       Characterizations='characterizations',
+                                       ReductionProperties='__pd_reduction_properties')
+manager = mantid.PropertyManagerDataService.retrieve('__pd_reduction_properties')
+simpleapi.Rebin(InputWorkspace=input, OutputWorkspace=input,
+                Params=(manager['tof_min'].value,100,manager['tof_max'].value))
+
+
+if True: #input.getNumberEvents() > 0:
+    simpleapi.AlignAndFocusPowder(InputWorkspace=input, OutputWorkspace=output,
+                                  CalFilename=cal_file,
+                                  Params=-0.0008,
+                                  RemovePromptPulseWidth=0, # should be 50
+                                  ReductionProperties='__pd_reduction_properties')
+    simpleapi.ConvertUnits(InputWorkspace=output, OutputWorkspace=output,
+                           Target='dSpacing', EMode='Elastic')
