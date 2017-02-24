cc4520b11c75b8abfea041f6abd63918f31b38c9
mantid-publisher <mantid-developers@mantidproject.org>
2017-02-24 13:25:03 -0500

    Updated reduction script by cron job

--- ReductionScripts/sns/ref_m/REF_M_utils.py
+++ ReductionScripts/sns/ref_m/REF_M_utils.py
@@ -30,8 +30,10 @@ def reduce_data(run_number):
         
     apply_norm = True
     if norm_run is None:
-        logging.error("Could not find direct beam run: skipping")
+        logging.notice("Could not find direct beam run: skipping")
         apply_norm = False
+    else:
+        logging.notice("Direct beam run: %s" % norm_run)
 
     # Find peak in direct beam run
     ws = LoadEventNexus(Filename="REF_M_%s" % norm_run,
