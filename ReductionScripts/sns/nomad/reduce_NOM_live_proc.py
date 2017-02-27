403a8d32cdf221b2f130bc2b48345fb56bb4b162
mantid-publisher <mantid-developers@mantidproject.org>
2017-02-27 15:19:03 -0500

    Updated reduction script by cron job

--- ReductionScripts/sns/nomad/reduce_NOM_live_proc.py
+++ ReductionScripts/sns/nomad/reduce_NOM_live_proc.py
@@ -5,10 +5,16 @@ import os
 cal_dir = '/SNS/NOM/shared/CALIBRATION/2016_2_1B_CAL/'
 cal_file  = "/SNS/NOM/IPTS-18316/shared/NOM_calibrate_d87646_2016_12_14.h5" #os.path.join(cal_dir, 'NOM_d85746_2016_11_21_shifter.h5')
 char_file = os.path.join(cal_dir, 'NOM_char_2016_08_18-rietveld.txt')
+expiniFileDefault = "/SNS/NOM/IPTS-18316/shared/autoNOM/exp.ini"
+expiniFilename = os.path.join('/SNS/NOM/IPTS-18478/', 'shared', 'autoNOM', 'exp.ini')
+if not os.path.exists(expiniFilename):
+    expiniFilename = expiniFileDefault
+
 
 mantid.logger.information('Number events = %d' % input.getNumberEvents())
 
 simpleapi.PDLoadCharacterizations(Filename=char_file,
+                                  ExpIniFilename=expiniFilename,
                                   OutputWorkspace='characterizations')
 simpleapi.PDDetermineCharacterizations(InputWorkspace=input,
                                        Characterizations='characterizations',
