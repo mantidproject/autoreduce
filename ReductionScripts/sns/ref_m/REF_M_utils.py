a6bc27dfc55f55b991a6481e28f9ddf6764f1507
mantid-publisher <mantid-developers@mantidproject.org>
2017-02-27 16:13:04 -0500

    Updated reduction script by cron job

--- ReductionScripts/sns/ref_m/REF_M_utils.py
+++ ReductionScripts/sns/ref_m/REF_M_utils.py
@@ -219,6 +219,8 @@ def guess_params(ws, tolerance=0.02):
     
     dangle_ = abs(ws.getRun().getProperty("DANGLE").getStatistics().mean)
     is_direct_beam = dangle_ < tolerance
+    if is_direct_beam:
+        logging.info("Direct beam run with DANGLE = %s" % dangle)
     return peak, low_res, peak_position, is_direct_beam
 
 if __name__ == '__main__':
