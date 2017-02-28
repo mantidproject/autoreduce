3932b488e8c06447512e5c20a98009c2eab0bb92
mantid-publisher <mantid-developers@mantidproject.org>
2017-02-28 12:40:05 -0500

    Updated reduction script by cron job

--- ReductionScripts/sns/ref_m/REF_M_utils.py
+++ ReductionScripts/sns/ref_m/REF_M_utils.py
@@ -158,12 +158,12 @@ def find_direct_beam(scatt_ws, tolerance=0.02, skip_slits=False, allow_later_run
             if not os.path.isfile(summary_path):
                 is_valid = False
                 try:
-                    for entry in ['Off_Off', 'On_Off', 'Off_On', 'On_On']:
+                    for entry in ['entry', 'entry-Off_Off', 'entry-On_Off', 'entry-Off_On', 'entry-On_On']:
                         ws = LoadEventNexus(Filename=os.path.join(data_dir, item),
-                                            NXentryName='entry-%s' % entry,
+                                            NXentryName=entry,
                                             MetaDataOnly=True,
                                             OutputWorkspace="meta_data")
-                        if ws.getNumberEvents() > 10000:
+                        if ws.getNumberEvents() > 1000:
                             is_valid = True
                             break
                 except:
