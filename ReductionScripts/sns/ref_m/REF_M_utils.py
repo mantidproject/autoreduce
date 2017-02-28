6fe90200a23534f2bdc497a3220ecd924e50e18e
mantid-publisher <mantid-developers@mantidproject.org>
2017-02-28 10:22:03 -0500

    Updated reduction script by cron job

--- ReductionScripts/sns/ref_m/REF_M_utils.py
+++ ReductionScripts/sns/ref_m/REF_M_utils.py
@@ -40,9 +40,11 @@ def reduce_data(run_number):
 
     try:
         from postprocessing.publish_plot import plot1d
-        plot1d(run_number, data_list, data_names=data_names, instrument='REF_M',
-                   x_title=u"Q (1/\u212b)", x_log=True,
-                   y_title="Reflectivity", y_log=True, show_dx=False)
+        if len(data_list) > 0:
+            plot1d(run_number, data_list, data_names=data_names, instrument='REF_M',
+                       x_title=u"Q (1/\u212b)", x_log=True,
+                       y_title="Reflectivity", y_log=True, show_dx=False)
+        logging.warning("Nothing to plot")
     except:
         logging.error(str(sys.exc_value))
         logging.error("No publisher module found")
