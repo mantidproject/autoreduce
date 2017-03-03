d6300b29a86420485e104fd61a548d3ae5873106
mantid-publisher <mantid-developers@mantidproject.org>
2017-03-03 09:06:04 -0500

    Updated reduction script by cron job

--- ReductionScripts/sns/nomad/reduce_NOM_live_proc.py
+++ ReductionScripts/sns/nomad/reduce_NOM_live_proc.py
@@ -4,7 +4,7 @@ import os
 
 cal_dir = '/SNS/NOM/shared/CALIBRATION/2016_2_1B_CAL/'
 cal_file  = "/SNS/NOM/IPTS-18316/shared/NOM_calibrate_d87646_2016_12_14.h5" #os.path.join(cal_dir, 'NOM_d85746_2016_11_21_shifter.h5')
-char_file = os.path.join(cal_dir, 'NOM_char_2016_08_18-rietveld.txt')
+char_file = os.path.join(cal_dir, 'NOM_char_2016_12-13-rietveld.txt')
 expiniFileDefault = "/SNS/NOM/IPTS-18316/shared/autoNOM/exp.ini"
 expiniFilename = os.path.join('/SNS/NOM/IPTS-17982/', 'shared', 'autoNOM', 'exp.ini')
 if not os.path.exists(expiniFilename):
