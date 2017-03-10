520fc5d1ce519ae64499e5200f273e7f03e82fda
mantid-publisher <mantid-developers@mantidproject.org>
2017-03-10 16:20:03 -0500

    Updated reduction script by cron job

--- ReductionScripts/sns/corelli/reduce_CORELLI.py
+++ ReductionScripts/sns/corelli/reduce_CORELLI.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import sys,os
-sys.path.append("/opt/Mantid/bin")
+sys.path.append("/opt/mantidnightly/bin")
 
 from mantid.simpleapi import *
 from mantid import logger
@@ -15,10 +15,10 @@ import matplotlib.pyplot as plt
 class processInputs(object):
     def __init__(self):
         #templated stuff
-        self.ub_matrix_file='/SNS/CORELLI/IPTS-18149/shared/DTO_UB.mat' #'/SNS/CORELLI/IPTS-12310/shared/Sr214-Tb1000-2nd-20150512/UB-H0L-may12.mat'
+        self.ub_matrix_file='' #'/SNS/CORELLI/IPTS-12310/shared/Sr214-Tb1000-2nd-20150512/UB-H0L-may12.mat'
         self.vanadium_SA_file='/SNS/CORELLI/shared/Vanadium/2017A/Spectrum_SlimSam_20170307_Central.nxs' #'/SNS/CORELLI/shared/Vanadium/SolidAngle20150411.nxs'
         self.vanadium_flux_file='/SNS/CORELLI/shared/Vanadium/2017A/SolidAngle_SlimSam_20170307_Central.nxs' #'/SNS/CORELLI/shared/Vanadium/Spectrum20150411.nxs'
-        self.mask=[{'Bank': '1-30,62-91'}] #[{'Tube':'1,2,3,4','Bank':'','Pixel':''}]
+        self.mask=[] #[{'Tube':'1,2,3,4','Bank':'','Pixel':''}]
         self.plot_requests=[{'Minimum': '-0.15', 'PerpendicularTo': '[0,K,0]', 'Maximum': '0.15'}] #[{'PerpendicularTo':"[0,K,0]",'Minimum':'-0.05','Maximum':'0.05'},{'PerpendicularTo':"[0,K,0]",'Minimum':'10.95','Maximum':'11.05'},{'PerpendicularTo':"[0,K,0]",'Minimum':'0.95','Maximum':'1.05'}]
         self.useCC='True' #"True"
         #other
