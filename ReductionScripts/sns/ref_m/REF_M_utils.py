efbf318d9289f408fa0c8929c86176924a74b9b3
mantid-publisher <mantid-developers@mantidproject.org>
2017-04-06 14:51:03 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/ref_m/REF_M_utils.py
+++ ReductionScripts/sns/ref_m/REF_M_utils.py
@@ -583,7 +583,9 @@ def report(run_number, entry, reflectivity=None):
     # X-Y plot
     signal = np.log10(mtd['MR_%s' % run_number].extractY())
     z=np.reshape(signal, (n_x, n_y))
-    xy_plot = _plot2d(z=z.T, x=range(n_x), y=range(n_y), x_range=scatt_peak, y_range=scatt_low_res)
+    xy_plot = _plot2d(z=z.T, x=range(n_x), y=range(n_y),
+                      x_range=scatt_peak, y_range=scatt_low_res,
+                      title="r%s [%s]" % (run_number, entry))
 
     # X-TOF plot
     tof_min = mtd['MR_%s' % run_number].getTofMin()
@@ -598,8 +600,9 @@ def report(run_number, entry, reflectivity=None):
     tof_axis = direct_summed.extractX()[0]/1000.0
 
     x_tof_plot = _plot2d(z=signal, y=range(signal.shape[0]), x=tof_axis,
-            x_range=None, y_range=scatt_peak,
-            x_label="TOF (ms)", y_label="X pixel")
+                         x_range=None, y_range=scatt_peak,
+                         x_label="TOF (ms)", y_label="X pixel",
+                         title="r%s [%s]" % (run_number, entry))
     return [xy_plot, x_tof_plot]
 
 def get_meta_data(ws):
