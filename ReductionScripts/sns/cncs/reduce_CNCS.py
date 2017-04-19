c388a3e73172a309b7838fe5f7b9a925e94e7db8
mantid-publisher <mantid-developers@mantidproject.org>
2017-04-19 09:29:03 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/cncs/reduce_CNCS.py
+++ ReductionScripts/sns/cncs/reduce_CNCS.py
@@ -10,18 +10,18 @@ from mantid.simpleapi import *
 #parameters section
 #this part changes with web input
 MaskBTPParameters=[]
-MaskBTPParameters.append({'Pixel': '1-40'})
-MaskBTPParameters.append({'Pixel': '89-128'})
+MaskBTPParameters.append({'Pixel': '1-42'})
+MaskBTPParameters.append({'Pixel': '96-128'})
 MaskBTPParameters.append({'Bank': '36-50'})
 
 #MaskBTPParameters.append({'Pixel': '1-43,95-128'})
 #MaskBTPParameters.append({'Pixel': '1-7,122-128'})
 #MaskBTPParameters.append({'Bank': '36-50'})#8T magnet
-raw_vanadium="/SNS/CNCS/IPTS-16111/6/208719/NeXus/CNCS_208719_event.nxs"
-processed_vanadium="van208719.nxs"
+raw_vanadium="/SNS/CNCS/IPTS-18132/0/215731/NeXus/CNCS_215731_event.nxs"
+processed_vanadium="van215731.nxs"
 VanadiumIntegrationRange=[49500.0,50500.0]#integration range for Vanadium in TOF at 1.0 meV
 grouping="8x1" #allowed values 1x1, 2x1, 4x1, 8x1, 8x2 powder
-Emin="-0.95"
+Emin="-0.1"
 Emax="0.95"
 Estep="0.005"
 E_pars_in_mev=False
