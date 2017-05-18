d554bd8bb8c509259290605b624c7adce30e24e6
mantid-publisher <mantid-developers@mantidproject.org>
2017-05-18 11:22:02 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/cncs/reduce_CNCS.py
+++ ReductionScripts/sns/cncs/reduce_CNCS.py
@@ -18,9 +18,9 @@ MaskBTPParameters.append({'Bank': '36-50'})
 #MaskBTPParameters.append({'Pixel': '1-7,122-128'})
 #MaskBTPParameters.append({'Bank': '36-50'})#8T magnet
 raw_vanadium="/SNS/CNCS/IPTS-18132/0/215731/NeXus/CNCS_215731_event.nxs"
-processed_vanadium="van215731.nxs"
+processed_vanadium="powder_van_215731.nxs"
 VanadiumIntegrationRange=[49500.0,50500.0]#integration range for Vanadium in TOF at 1.0 meV
-grouping="8x1" #allowed values 1x1, 2x1, 4x1, 8x1, 8x2 powder
+grouping="powder" #allowed values 1x1, 2x1, 4x1, 8x1, 8x2 powder
 Emin="-0.1"
 Emax="0.95"
 Estep="0.005"
@@ -30,7 +30,7 @@ TIB_max=""
 T0=""
 Motor_names="huber,SERotator2,OxDilRot,CCR13VRot,SEOCRot,CCR10G2Rot,Ox2WeldRot,ThreeSampleRot"
 Temperature_names="SampleTemp,sampletemp,SensorB,SensorA,temp5,temp8,sensor0normal,SensorC"
-create_elastic_nxspe=True #+-0.1Ei, 5 steps
+create_elastic_nxspe=False #+-0.1Ei, 5 steps
 create_MDnxs=False
 a="6.802"
 b="8.875"
