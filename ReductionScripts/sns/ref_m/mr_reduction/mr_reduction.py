8bce4395472316fab0a2ba9dcbff8dec4f16267c
mantid-publisher <mantid-developers@mantidproject.org>
2017-07-20 15:30:05 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/ref_m/mr_reduction/mr_reduction.py
+++ ReductionScripts/sns/ref_m/mr_reduction/mr_reduction.py
@@ -82,6 +82,7 @@ class ReductionProcess(object):
             logging.error(str(sys.exc_value))
 
         # Generate report and script
+        logging.info("Processing collection of %s reports", len(report_list))
         html_report, script = process_collection(summary_content=ref_plot, report_list=report_list, publish=True)
 
         try:
