1f7e650d67c11fc5fcc7cfbabcdfb830ad59bc31
mantid-publisher <mantid-developers@mantidproject.org>
2017-07-21 10:49:04 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/powgen/sumRun_PG3.py
+++ ReductionScripts/sns/powgen/sumRun_PG3.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
-import nxs, os, numpy, sys, posixpath, logging, csv
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+import h5py
+import os
 import sys
-sys.path.append("/opt/Mantid/bin")
+import numpy
+import csv
+import re
+try:
+    import ConfigParser as configparser
+except ImportError:
+    import configparser
 
-#import xml.utils.iso8601, ConfigParser
-import ConfigParser
-from datetime import datetime
 
 class RunInfo:
     def __init__(self, instrument, infilename):
@@ -13,9 +18,11 @@ class RunInfo:
         self._nodes = []
         self._values = []
         self._infilename = infilename
-        config_path = '/SNS/' + instrument + '/shared/autoreduce/sumRun_' + instrument + '.cfg' 
-        print config_path
-        config = ConfigParser.SafeConfigParser()
+        config_path = '/SNS/' + instrument + '/shared/autoreduce/sumRun_' + instrument + '.cfg'
+        if not os.path.exists(config_path):
+            raise RuntimeError('Failed to find config "%s"' % config_path)
+        print('loading config:', config_path)
+        config = configparser.SafeConfigParser()
         config.read(config_path)
         self._names = config.get("header", "names").split(',')
 
@@ -23,41 +30,43 @@ class RunInfo:
             self._nodes.append(config.get('node', name))
 
     def fillRunData(self):
-        #open nexus file
-        file = nxs.open(self._infilename, 'r')
-        for node in self._nodes:
-            try:
-                file.openpath(node)
-                value =  file.getdata()
-                if isinstance(value, numpy.ndarray):
-                    if value.shape[0] == 1:
-                        value = value[0]
-                    if isinstance(value, numpy.string_):
-                        value = str(value)
-                    else:
-                        value = sum(value)
-            except Exception as e:
-                print e 
-                value = 'N/A' 
-                
-            self._values.append(str(value))
-        file.close()
- 
+        # open nexus file
+        with h5py.File(self._infilename, 'r') as f:
+            for node in self._nodes:
+                try:
+                    value = f[node].value
+                    if isinstance(value, numpy.ndarray):
+                        if value.shape == (1,):
+                            value = value[0]
+                        else:
+                            value = sum(value)
+                        if isinstance(value, bytes):
+                            value = value.decode().replace(',','')
+                        if 'time' in node.lower():
+                            value = re.sub('\..*','',value) # remove fractional seconds
+                            value = re.sub('-','/',value) # change  '-' to '/'
+                            value = re.sub('T','/',value) # change  'T' to '/'
+                except Exception as e:
+                    print(e)
+                    value = 'N/A'
+                self._values.append(str(value))
+
     def getNames(self):
         return self._names
 
     def getValues(self):
         return self._values
 
+
 if __name__ == "__main__":
     # set up the options
     if len(sys.argv) != 4:
-        print "run_info takes 3 arguments: instrument, nexus file and output file. Exiting..."
+        print("run_info takes 3 arguments: instrument, nexus file and output file. Exiting...")
         sys.exit(-1)
 
-    print sys.argv[1]
-    print sys.argv[2]
-    print sys.argv[3]
+    print(sys.argv[1])
+    print(sys.argv[2])
+    print(sys.argv[3])
     runInfo = RunInfo(sys.argv[1], sys.argv[2])
     runInfo.fillRunData()
 
@@ -71,7 +80,7 @@ if __name__ == "__main__":
         f = open(outfile, 'w')
         writer = csv.writer(f)
         handle = open(outfile, "w")
-        # get header information for the csv file 
+        # get header information for the csv file
         writer.writerow(runInfo.getNames())
 
     writer.writerow(runInfo.getValues())
