19cd1f8597b5f1a6070f1a15eeff1cf86ceb3b80
mantid-publisher <mantid-developers@mantidproject.org>
2017-08-29 21:00:03 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/powgen/reduce_PG3_live_proc.py
+++ ReductionScripts/sns/powgen/reduce_PG3_live_proc.py
@@ -4,8 +4,8 @@ import os
 
 # get information from autoreduction
 cal_dir = "/SNS/PG3/shared/CALIBRATION/2017_1_2_11A_CAL/"
-cal_file  = os.path.join(cal_dir, 'PG3_OC_d38340_2017_08_23_BANK1.h5')
-char_backgrounds = os.path.join(cal_dir, "PG3_char_2017_08_23-HR-OC.txt")
+cal_file  = os.path.join(cal_dir, 'PG3_PAC_d37861_2017_08_08_BANK1.h5')
+char_backgrounds = os.path.join(cal_dir, "PG3_char_2017_08_08-HR-PAC.txt")
 char_bank1 = os.path.join(cal_dir, "PG3_char_2017_08_08-HR-BANK1.txt")
 
 mantid.logger.information('Number events = %d' % input.getNumberEvents())
