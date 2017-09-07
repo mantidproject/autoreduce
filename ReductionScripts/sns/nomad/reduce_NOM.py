a06e719bdc86bee95d73ac1c5b28cadad1c6e72a
mantid-publisher <mantid-developers@mantidproject.org>
2017-09-07 09:25:04 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/nomad/reduce_NOM.py
+++ ReductionScripts/sns/nomad/reduce_NOM.py
@@ -1,7 +1,6 @@
 from __future__ import (absolute_import, division, print_function)
 import os
 import sys
-import shutil
 sys.path.append("/opt/mantidnightly/bin")
 from mantid.simpleapi import *
 import mantid
