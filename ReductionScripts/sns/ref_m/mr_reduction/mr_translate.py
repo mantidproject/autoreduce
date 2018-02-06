3fe44d361202bed4d2a08f5fbbcc32df905c5192
mantid-publisher <mantid-developers@mantidproject.org>
2018-02-06 15:07:03 -0500

    Updated reduction script by cron job

--- ReductionScripts/sns/ref_m/mr_reduction/mr_translate.py
+++ ReductionScripts/sns/ref_m/mr_reduction/mr_translate.py
@@ -52,6 +52,8 @@ import os
 import nxs
 from nxs import NXfield, NXgroup
 import numpy as np
+import mr_filter_events
+
 
 def translate_entry(raw_event_file, filtered_file, entry_name, histo=True):
     """
@@ -79,18 +81,23 @@ def translate_entry(raw_event_file, filtered_file, entry_name, histo=True):
     nx_quick.end_time = tree_raw[entry_].end_time
 
     # DAS logs
-    for item in tree_processed.NXentry[0].logs.keys():
-        if not hasattr(tree_processed.NXentry[0].logs[item], 'value'):
+    for key, nx_object in tree_processed.NXentry[0].logs.items():
+        item_out = key
+        toks = key.split(':')
+        if len(toks)>1:
+            item_out = "%s_%s" % (toks[-2], toks[-1])
+
+        if not hasattr(nx_object, 'value'):
             continue
-        if isinstance(tree_processed.NXentry[0].logs[item].value.dtype, str):
+        if isinstance(nx_object.value.dtype, str):
             continue
 
-        exec("nx_quick.DASlogs.%s = NXgroup(name='%s', nxclass='NXlog')" % (item, item))
-        exec("value = tree_processed.NXentry[0].logs.%s.value.nxdata" % item)
-        exec("nx_quick.DASlogs.%s.value = NXfield(name='value', dtype='float64', value=value)" % item)
-        if hasattr(tree_processed.NXentry[0].logs[item], 'time'):
-            exec("time = tree_processed.NXentry[0].logs.%s.time.nxdata" % item)
-            exec("nx_quick.DASlogs.%s.time = NXfield(name='time', dtype='float64', value=time)" % item)
+        exec("nx_quick.DASlogs.%s = NXgroup(name='%s', nxclass='NXlog')" % (item_out, item_out))
+        log_value = nx_object.value.nxdata
+        exec("nx_quick.DASlogs.%s.value = NXfield(name='value', dtype='float64', value=log_value)" % item_out)
+        if hasattr(nx_object, 'time'):
+            log_time = nx_object.time.nxdata
+            exec("nx_quick.DASlogs.%s.time = NXfield(name='time', dtype='float64', value=log_time)" % item_out)
 
     # Proton charge from Mantid is in uAh. In the raw file it's pC.
     nx_quick.proton_charge = NXfield(name='proton_charge', dtype='float64',
@@ -99,34 +106,32 @@ def translate_entry(raw_event_file, filtered_file, entry_name, histo=True):
     nx_quick.duration = NXfield(name='duration', dtype='float64', value=tree_processed.NXentry[0].logs.duration.value.nxdata)
 
     # Instrument
-    nx_quick.instrument.SNS = tree_raw[entry_].instrument.SNS
-    nx_quick.instrument.SNSdetector_calibration_id = tree_raw[entry_].instrument.SNSdetector_calibration_id
-    nx_quick.instrument.SNSgeometry_file_name = tree_raw[entry_].instrument.SNSgeometry_file_name
-    nx_quick.instrument.SNSmotor_file_name = tree_raw[entry_].instrument.SNSmotor_file_name
-    nx_quick.instrument.SNSnxtranslate = tree_raw[entry_].instrument.SNSnxtranslate
-    nx_quick.instrument.SNStranslation_service = tree_raw[entry_].instrument.SNStranslation_service
+    nx_quick.instrument.SNSgeometry_file_name = NXfield(name='SNSgeometry_file_name', value='')
 
-    nx_quick.instrument.aperture1.distance = NXfield(name='distance', dtype='float64', value=tree_raw[entry_].instrument.aperture1.distance)
-    nx_quick.instrument.aperture2.distance = NXfield(name='distance', dtype='float64', value=tree_raw[entry_].instrument.aperture2.distance)
-    nx_quick.instrument.aperture3.distance = NXfield(name='distance', dtype='float64', value=tree_raw[entry_].instrument.aperture3.distance)
+    nx_quick.instrument.aperture1.distance = -2.599
+    nx_quick.instrument.aperture2.distance = -2.019
+    nx_quick.instrument.aperture3.distance = -0.714
 
     # Bank1 data
-    nx_quick.instrument.bank1.origin = tree_raw[entry_].instrument.bank1.origin
-    nx_quick.instrument.bank1.x_pixel_offset = tree_raw[entry_].instrument.bank1.x_pixel_offset
-    nx_quick.instrument.bank1.y_pixel_offset = tree_raw[entry_].instrument.bank1.y_pixel_offset
-
-    nx_quick.SNSproblem_log_geom = tree_raw[entry_].SNSproblem_log_geom
-
+    nx_quick.instrument.bank1.origin = NXgroup(name='origin', nxclass='NXGroup')
+    nx_quick.instrument.bank1.origin.shape = NXgroup(name='shape', nxclass='NXGroup')
+    nx_quick.instrument.bank1.origin.shape.size = NXfield(name='size', value=[0.212800, 0.179200])
+    
     nx_quick.experiment_identifier = tree_raw[entry_].experiment_identifier
     nx_quick.run_number = tree_raw[entry_].run_number
-    nx_quick.SNSproblem_log_geom.data = tree_raw[entry_].SNSproblem_log_geom.data
+    
+    nx_quick.SNSproblem_log_geom = NXgroup(name='SNSproblem_log_geom', nxclass='NXGroup')
+    nx_quick.SNSproblem_log_geom.data = NXfield(name='data', value='')
 
     if histo:
         create_histo(tree_processed, nx_quick)
     else:
         create_events(tree_processed, nx_quick)
 
-    return create_links(nx_quick)
+    if tree_processed.NXentry[0].logs.gd_prtn_chrg.value.nxdata>0:
+        return create_links(nx_quick)
+    else:
+        return nx_quick
 
 def create_histo(tree_processed, nx_quick):
     """
@@ -183,7 +188,10 @@ def create_events(tree_processed, nx_quick):
         event_ids.extend(n_events*[i])
 
     # In the raw SNS nexus file, event_id is a pixel ID
-    nx_quick.bank1_events.event_id =  NXfield(name='event_id', dtype='uint32', value=event_ids)
+    if len(event_ids) == 0:
+        nx_quick.bank1_events.event_id =  NXfield(name='event_id', dtype='uint32', value=0)
+    else:
+        nx_quick.bank1_events.event_id =  NXfield(name='event_id', dtype='uint32', value=event_ids)
     nx_quick.total_counts = NXfield(name='total_counts', dtype='uint64', value=len(event_ids))
     # Empty detector counts
     nx_quick.instrument.bank1.data_x_y = NXfield(name='data_x_y', dtype='uint32',
@@ -215,6 +223,8 @@ def create_links(nx_quick):
     """
     nx_quick.sample.SANGLE = nx_quick.DASlogs.SANGLE
 
+    if "AnalyzerTrans" in nx_quick.DASlogs.keys():
+        nx_quick.instrument.analyzer.AnalyzerTrans = nx_quick.DASlogs.AnalyzerTrans
     nx_quick.instrument.analyzer.AnalyzerLift = nx_quick.DASlogs.AnalyzerLift
     nx_quick.instrument.analyzer.AnalyzerRot = nx_quick.DASlogs.AnalyzerRot
     nx_quick.instrument.polarizer.PolLift = nx_quick.DASlogs.PolLift
@@ -229,36 +239,38 @@ def create_links(nx_quick):
     nx_quick.instrument.bank1.DIRPIX = nx_quick.DASlogs.DIRPIX
     nx_quick.instrument.bank1.SampleDetDis = nx_quick.DASlogs.SampleDetDis
 
-    nx_quick.bank1.x_pixel_offset = nx_quick.instrument.bank1.x_pixel_offset
-    nx_quick.bank1.y_pixel_offset = nx_quick.instrument.bank1.y_pixel_offset
+    #nx_quick.bank1.x_pixel_offset = nx_quick.instrument.bank1.x_pixel_offset
+    #nx_quick.bank1.y_pixel_offset = nx_quick.instrument.bank1.y_pixel_offset
     nx_quick.bank1.data_x_y = nx_quick.instrument.bank1.data_x_y
 
     return nx_quick
 
-def filter_events(raw_event_file, histo=True):
-    """
-        Filter an event nexus file
-        :param str raw_event_file: name of the event data file
-    """
-    if histo:
-        return {'entry-Off_Off':'filtered_histo.nxs', 'entry-On_Off':'filtered_histo.nxs'}
-    else:
-        return {'entry-Off_Off':'filtered.nxs', 'entry-On_Off':'filtered.nxs'}
-
-def translate(raw_event_file, identifier='quick', histo=True):
+def translate(raw_event_file, identifier='quick', events=True, histo=True, sub_dir=None):
     """
         Translate an event nexus file
         :param str raw_event_file: name of the event data file to process
         :param str identifier: suffix for the output data file
     """
     # Create a filtered file
-    filtered_files = filter_events(raw_event_file, histo=histo)
+    xs_event_files, xs_histo_files = mr_filter_events.filter_cross_sections(raw_event_file, events=events, histo=histo)
+    print xs_event_files
+    print xs_histo_files
+    if events:
+        process(raw_event_file, xs_event_files, histo=False, sub_dir=sub_dir)
+    if histo:
+        process(raw_event_file, xs_histo_files, histo=True, sub_dir=sub_dir)
 
+def process(raw_event_file, filtered_files, histo=False, sub_dir=None):
     # Assemble the entries
     tree = nxs.NXroot()
     for entry_name, filtered_file in filtered_files.items():
+        if filtered_file is None:
+            continue
         entry = translate_entry(raw_event_file, filtered_file , entry_name=entry_name, histo=histo)
         tree.insert(entry)
+
+        if os.path.isfile(filtered_file):
+            os.remove(filtered_file)
 
     # Create a name that QuickNXS will recognize
     if raw_event_file.endswith('_event.nxs'):
@@ -271,6 +283,11 @@ def translate(raw_event_file, identifier='quick', histo=True):
             output_file = raw_event_file.replace('.nxs.h5', '_histo.nxs')
         else:
             output_file = raw_event_file.replace('.nxs.h5', '_event.nxs')
+
+    if sub_dir is not None:
+        dir_, file_ = os.path.split(output_file)
+        output_file = os.path.join(dir_, sub_dir, file_)
+
     if os.path.isfile(output_file):
         os.remove(output_file)
 
@@ -278,9 +295,18 @@ def translate(raw_event_file, identifier='quick', histo=True):
 
 
 if __name__ == '__main__':
-    # Example data files
-    # /SNS/REF_M/IPTS-17695/data/REF_M_26403_histo.nxs
-    # /SNS/REF_M/IPTS-17695/data/REF_M_26403_event.nxs
-    translate('REF_M_26403_event.nxs', histo=False)
-    translate('REF_M_26403_event.nxs', histo=True)
+    import time
+    # Test directory: /SNS/REF_M/shared/ADARA.Test.Data.2018
 
+    #translate('/SNS/REF_M/shared/ADARA.Test.Data.2018/REF_M_25631.nxs.h5', histo=False, sub_dir='translation_output')
+    #translate('/SNS/REF_M/shared/ADARA.Test.Data.2018/REF_M_28722.nxs.h5', histo=False, sub_dir='translation_output')
+    translate('/SNS/REF_M/shared/ADARA.Test.Data.2018/REF_M_28144.nxs.h5', histo=False, sub_dir='translation_output')
+    
+    if False:
+        data_dir = '/SNS/REF_M/shared/ADARA.Test.Data.2018'
+        for item in os.listdir(data_dir):
+            if os.path.isfile(os.path.join(data_dir, item)) and item.endswith('nxs.h5'):
+                print("\n%s\n" % item)
+                t_0 = time.time()
+                translate(os.path.join(data_dir, item), histo=False, sub_dir='translation_output')
+                print("%s: %s sec" % (item, time.time()-t_0))
