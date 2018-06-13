8a6ac508f4d3be56425fe6c5b50d890a2c56cac7
mantid-publisher <mantid-developers@mantidproject.org>
2018-06-13 00:12:04 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/cncs/reduce_CNCS.py
+++ ReductionScripts/sns/cncs/reduce_CNCS.py
@@ -40,7 +40,7 @@ beta="90.0"
 gamma="90.0"
 uVector="1,0,0"
 vVector="0,0,1"
-sub_directory="/SNS/CNCS/IPTS-20360/shared/autoreduce/NanoTriton/"
+sub_directory="/SNS/CNCS/IPTS-21088/shared/autoreduce/NanoTriton/"
 
 #parameters not on the webpage
 #below remains unchanged
