e616a53124ba2d42fbd6d8af1ec2e87d732102a1
mantid-publisher <mantid-developers@mantidproject.org>
2018-07-04 10:21:03 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/powgen/reduce_PG3.py
+++ ReductionScripts/sns/powgen/reduce_PG3.py
@@ -7,8 +7,8 @@ sys.path.append("/opt/mantidnightly/bin")
 from mantid.simpleapi import *
 import mantid
 cal_dir = '/SNS/PG3/shared/CALIBRATION/2018_2_11A_CAL/'
-cal_file  = os.path.join(cal_dir,'PG3_PAC_d40481_2018_06_13.h5') # contains ALL grouping
-char_backgrounds = os.path.join(cal_dir, "PG3_char_2018_06_11-HighInt.txt")
+cal_file  = os.path.join(cal_dir,'PG3_OC_d40895_2018_07_03.h5') # contains ALL grouping
+char_backgrounds = os.path.join(cal_dir, "PG3_char_2018_07_04-HighInt-OC.txt")
 
 char_inplane = os.path.join(cal_dir, "PG3_char_2018_05_26.txt")
 group_inplane = os.path.join(cal_dir, 'grouping', 'PG3_Grouping-IP.xml')
