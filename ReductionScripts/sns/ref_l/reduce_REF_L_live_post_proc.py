c9f01f91dd3b8cdd0faa78bcb4fe1d741d8de940
mantid-publisher <mantid-developers@mantidproject.org>
2018-07-05 16:21:04 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/ref_l/reduce_REF_L_live_post_proc.py
+++ ReductionScripts/sns/ref_l/reduce_REF_L_live_post_proc.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import mantid.simpleapi as api
 import numpy as np
 
@@ -169,7 +170,7 @@ def reduce_data(ws):
     #-------------------------------------------------------------------------
     # Produce plot for the web monitor
     default_file_name = 'REFL_%s_combined_data_auto.txt' % first_run_of_set
-    default_file_path = os.path.join(output_dir. default_file_name)
+    default_file_path = os.path.join(output_dir, default_file_name)
     if os.path.isfile(default_file_path):
         print("Loading %s" % os.path.join(output_dir, default_file_name))
         reflectivity = LoadAscii(Filename=default_file_path, Unit="MomentumTransfer")
