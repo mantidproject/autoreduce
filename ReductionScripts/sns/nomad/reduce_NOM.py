b4404db1e4adb6f3a7075abb8d3b1e4df14c86e3
mantid-publisher <mantid-developers@mantidproject.org>
2018-07-28 00:15:04 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/nomad/reduce_NOM.py
+++ ReductionScripts/sns/nomad/reduce_NOM.py
@@ -15,7 +15,7 @@ vanradius = 0.58
 #wavelengthMax = 2.9
 calFile = "/SNS/NOM/shared/CALIBRATION/2017_2_1B_CAL/NOM_d94214_2017_07_14_shifter.h5"
 charFile = "/SNS/NOM/shared/CALIBRATION/2018_2_1B_CAL/NOM_char_2018_05_29-rietveld.txt"
-expiniFileDefault = "/SNS/snfs1/instruments/NOM/IPTS-20572/shared/autoNOM_00/exp.ini"
+expiniFileDefault = "/SNS/snfs1/instruments/NOM/IPTS-20478/shared/autoNOM_2/exp.ini"
 # 0 means use the runs specified in the exp.ini
 # -1 means turn off the correction
 # specify files to be summed as a tuple or list
