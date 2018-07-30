a06aaec4d4ea20dedd101cb32fdc4154cfcc4985
mantid-publisher <mantid-developers@mantidproject.org>
2018-07-30 18:23:05 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/ref_m/reduce_REF_M.py
+++ ReductionScripts/sns/ref_m/reduce_REF_M.py
@@ -27,7 +27,7 @@ if __name__=="__main__":
     Options:
         Use SANGLE:       True
         Use Const-Q:      False
-        Fit peak in roi:  True
+        Fit peak in roi:  False
         Use bck ROI:      False
         Force peak:       False [162, 192]
         Force background: False [50, 100]
@@ -55,7 +55,7 @@ if __name__=="__main__":
                                 use_sangle=True,
                                 const_q_binning=False,
                                 const_q_cutoff=None,
-                                update_peak_range=True,
+                                update_peak_range=False,
                                 use_roi=True,
                                 use_roi_bck=False,
                                 force_peak_roi=False, peak_roi=[162, 192],
