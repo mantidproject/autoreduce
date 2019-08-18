c22c7402bef977795fe1e325f77764845ddb037c
mantid-publisher <mantid-developers@mantidproject.org>
2019-08-18 11:30:02 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/hyspec/reduce_HYS.py
+++ ReductionScripts/sns/hyspec/reduce_HYS.py
@@ -1,4 +1,7 @@
 import sys,os,glob, subprocess, shutil
+import os
+import subprocess
+from mantid.simpleapi import logger
 
 def do_reduction(path,outdir):
     reduction_files=glob.glob(os.path.join(outdir,'reduce_HYS_*.py'))
@@ -29,3 +32,28 @@ if __name__ == "__main__":
         path = sys.argv[1]
         out_dir = sys.argv[2]
         do_reduction(path, out_dir)
+        
+output_dir = '/home/3y9/temp/' #this is the original output directory passed to the autoreduction script
+output_scripts_dir = os.path.join(output_dir,'SCDGS_scripts')
+cwd = os.getcwd()
+# if folder is not there, clone the repository
+cmd = 'git clone --depth 1 -b master https://github.com/AndreiSavici/DGS_SC_scripts.git {}'.format(output_scripts_dir)
+if os.path.isdir(output_scripts_dir):
+    #pull the latest version of the scripts
+    os.chdir(output_scripts_dir)
+    cmd = 'git pull --rebase'
+
+proc = subprocess.Popen(cmd,
+                        shell=True,
+                        stdin=subprocess.PIPE,                               
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE,
+                        universal_newlines=True)
+out = proc.communicate()
+rc = proc.returncode
+if rc:
+    logger.error('single crystal scripts: ' + out[1])
+else:
+    logger.notice('single crystal scripts: ' + out[0])
+os.chdir(cwd)        
+        
