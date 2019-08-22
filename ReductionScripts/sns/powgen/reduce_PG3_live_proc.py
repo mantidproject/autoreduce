ed091417341964d722373a66ab5b9159d30c1f1b
mantid-publisher <mantid-developers@mantidproject.org>
2019-08-22 14:15:01 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/powgen/reduce_PG3_live_proc.py
+++ ReductionScripts/sns/powgen/reduce_PG3_live_proc.py
@@ -4,9 +4,9 @@ import os
 
 # get information from autoreduction
 cal_dir = '/SNS/PG3/shared/CALIBRATION/2019_2_11A_CAL/'
-cal_file  = os.path.join(cal_dir,'PG3_PAC_d43675_2019_06_25.h5') # contains ALL grouping
-char_backgrounds = os.path.join(cal_dir, "PG3_char_2019_07_31-HighRes-PAC_1.05mW.txt")
-char_inplane = os.path.join(cal_dir, "PG3_char_2019_06_25_PAC_limit.txt")
+cal_file  = os.path.join(cal_dir,'PG3_AGES_d44275_2019_08_22.h5') # contains ALL grouping
+char_backgrounds = os.path.join(cal_dir, "PG3_char_2019_8_22-HR-AGES.txt")
+char_inplane = os.path.join(cal_dir, "PG3_char_2019_08_22_AGES_limit.txt")
 
 ##### this will clear out the cache directory
 #filenames = [os.path.join('/tmp', item) for item in os.listdir('/tmp') if 'PG3_' in item]
