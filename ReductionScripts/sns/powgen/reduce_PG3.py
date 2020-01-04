ce96b4462bed2e333c3c23fcc609036179f2c843
mantid-publisher <mantid-developers@mantidproject.org>
2020-01-04 17:42:02 -0500

    Updated reduction script by cron job

--- ReductionScripts/sns/powgen/reduce_PG3.py
+++ ReductionScripts/sns/powgen/reduce_PG3.py
@@ -6,11 +6,10 @@ from sumRun_PG3 import addLineToCsv
 sys.path.append("/opt/mantidnightly/bin")
 from mantid.simpleapi import *
 import mantid
-cal_dir = '/SNS/users/qzg/data/SNS/PG3/shared/CALIBRATION/2020_1_11A_CAL/'
-cal_file  = os.path.join(cal_dir,'PG3_PAC_HR_d45453_2020_01_04.h5') # contains ALL grouping
-char_backgrounds = os.path.join(cal_dir, "PG3_char_2020_01_04-HighRes-PAC_1.4 MW.txt")
-char_inplane = os.path.join(cal_dir, "PG3_char_2020_01_04_PAC_limit_1.4MW.txt")
-
+cal_dir = '/SNS/PG3/shared/CALIBRATION/2019_2_11A_CAL/'
+cal_file  = os.path.join(cal_dir,'PG3_SlimSam_d45132_2019_12_11.h5') # contains ALL grouping
+char_backgrounds = os.path.join(cal_dir, "PG3_char_2019_06_25-HighRes-PAC_1.4 MW.txt")
+char_inplane = os.path.join(cal_dir, "PG3_char_2019_06_25_PAC_limit.txt")
 group_inplane = os.path.join(cal_dir, 'grouping', 'PG3_Grouping-IP.xml')
 binning = -0.0008
 QfitRange = [30.,50.]
