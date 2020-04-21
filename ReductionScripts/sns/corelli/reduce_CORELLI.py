545fe18359f1e3406fc09d2144f7052eff88d6aa
mantid-publisher <mantid-developers@mantidproject.org>
2020-04-21 16:56:03 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/corelli/reduce_CORELLI.py
+++ ReductionScripts/sns/corelli/reduce_CORELLI.py
@@ -295,6 +295,6 @@ if __name__ == "__main__":
 
     figfile.seek(0)
     figdata_png = base64.b64encode(figfile.getvalue())
-    div = '<img alt="{}" src="data:image/png;base64,{}" />'.format(output_file, figdata_png)
+    div = '<div><img alt="{}" src="data:image/png;base64,{}" /></div>'.format(output_file, figdata_png)
     request = publish_plot('CORELLI', runNumber, files={'file': div})
     print(request)
