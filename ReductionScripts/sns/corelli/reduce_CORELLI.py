f2602453700b6c4e27eb4bace00ed38f642a521e
mantid-publisher <mantid-developers@mantidproject.org>
2020-04-23 15:12:02 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/corelli/reduce_CORELLI.py
+++ ReductionScripts/sns/corelli/reduce_CORELLI.py
@@ -2,8 +2,8 @@
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 
 import sys,os
-#sys.path.insert(0,"/opt/mantid50/bin")
-#sys.path.insert(1,"/opt/mantid50/lib")
+sys.path.insert(0,"/opt/mantid50/bin")
+sys.path.insert(1,"/opt/mantid50/lib")
 
 from mantid.simpleapi import *
 from mantid import logger
