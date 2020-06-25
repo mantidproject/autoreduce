45b80cbba3a377e805d45bf8a6319a8f2343bc60
mantid-publisher <mantid-developers@mantidproject.org>
2020-06-25 15:13:03 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/ref_l/reduce_REF_L.py
+++ ReductionScripts/sns/ref_l/reduce_REF_L.py
@@ -63,9 +63,9 @@ elif os.path.isfile("/SNS/REF_L/shared/autoreduce/template.xml"):
 
 print("Using template: %s" % template_file)
 # Run the auto-reduction
-#ws = LoadEventNexus(Filename=event_file_path)
-output = LRAutoReduction(Filename=event_file_path,
-#                         InputWorkspace=ws,
+ws = LoadEventNexus(Filename=event_file_path)
+output = LRAutoReduction(#Filename=event_file_path,
+                         InputWorkspace=str(ws),
                          ScaleToUnity=NORMALIZE_TO_UNITY,
                          ScalingWavelengthCutoff=WL_CUTOFF,
                          PrimaryFractionRange=PRIMARY_FRACTION_RANGE,
