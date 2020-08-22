ba9d62621c08ad181054ccb95209e458b22bf4bb
mantid-publisher <mantid-developers@mantidproject.org>
2020-08-22 14:17:03 -0400

    Updated reduction script by cron job

--- ReductionScripts/sns/ref_m/reduce_REF_M.py
+++ ReductionScripts/sns/ref_m/reduce_REF_M.py
@@ -31,10 +31,10 @@ if __name__=="__main__":
         Use SANGLE:       False
         Use Const-Q:      False
         Fit peak in roi:  True
-        Use bck ROI:      True
+        Use bck ROI:      False
         Force peak:       False [176, 200]
-        Force background: True [75, 130]
-        Use side bck:     False
+        Force background: False [75, 130]
+        Use side bck:     True
         Bck width:        10
         Produce 2D plots  True
 
@@ -42,9 +42,9 @@ if __name__=="__main__":
         Const-Q cutoff:   None
 
 START_JSON
-{"use_sangle":False, "use_const_q":False, "fit_peak_in_roi":True, "use_roi_bck":True,
- "force_peak":False, "peak_min":176, "peak_max":200, "force_background":True,
- "bck_min":75, "bck_max":130, "use_side_bck":False, "bck_width":10
+{"use_sangle":False, "use_const_q":False, "fit_peak_in_roi":True, "use_roi_bck":False,
+ "force_peak":False, "peak_min":176, "peak_max":200, "force_background":False,
+ "bck_min":75, "bck_max":130, "use_side_bck":True, "bck_width":10
 }
 END_JSON
     """
@@ -64,9 +64,9 @@ END_JSON
                                 const_q_cutoff=None,
                                 update_peak_range=True,
                                 use_roi=True,
-                                use_roi_bck=True,
+                                use_roi_bck=False,
                                 force_peak_roi=False, peak_roi=[176, 200],
-                                force_bck_roi=True, bck_roi=[75, 130],
-                                use_tight_bck=False, bck_offset=10)
+                                force_bck_roi=False, bck_roi=[75, 130],
+                                use_tight_bck=True, bck_offset=10)
     red.plot_2d = True
     red.reduce()
